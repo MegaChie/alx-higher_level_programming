bfb2065fc4e3ae921ccf613eb6f893570c6e249a
MegaChie <workspace.mega@gmail.com>
2023-01-04 00:41:07 +0200

    Update 8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,8 @@
-#!/usr/bin/python3
 def uppercase(str):
     for count in range(len(str)):
         if(str[count] >= 'a' and str[count] <= 'z'):
-        tsm1 = tsm1 + chr((ord(str[count]) - 32))
-        else:
+            tsm1 = tsm1 + chr((ord(str[count]) - 32))
+            print("{}".format(ord(tsm1)))
+    else:
         tsm1 = tsm1 + str[count]
-    print("{}".format(str))
+        print("{}".format(ord(tsm1)))
