25d59ce95a24e160087f1cb950b984f82d20186c
MegaChie <workspace.mega@gmail.com>
2023-01-03 21:23:50 +0200

    Create 12-fizzbuzz.py

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def fizzbuzz():
+	for count in range(1,101):
+		if count % 3 == 0:
+			print("Fizz ", end = "")
+		elif count % 5 == 0:
+			print("Buzz ", end = "")
+		elif count % 5 == 0 and count % 3 == 0:
+			print("FizzBuzz ", end = "")
+		elif count == 100:
+			print("{}".format(count))
+		else:
+			print("{} ".format(count), end = "")
