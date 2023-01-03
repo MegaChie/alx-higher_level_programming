6242c0514effc80e86fe1116fe0787b26ee20346
MegaChie <workspace.mega@gmail.com>
2023-01-04 00:58:31 +0200

    Update 9-print_last_digit.py

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -4,5 +4,5 @@ def print_last_digit(number):
         lastDigit = (number * -1) % 10
     else:
         lastDigit = number % 10
-    print(number, end="")
+    print(lastDigit, end="")
     return lastDigit
