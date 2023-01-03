32e3e8fec3ecfc72ea0563db38ce7d7c411bdb07
MegaChie <workspace.mega@gmail.com>
2023-01-04 00:18:17 +0200

    Update 5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for count in range(0,100):
-  if count != 99:
-	print("{}".format(str(count).zfill(2)) ,end=", ")
-  else:
-	print ("{}".format(count))
+    if count != 99:
+        print("{}".format(str(count).zfill(2)) ,end=", ")
+    else:
+        print ("{}".format(count))
