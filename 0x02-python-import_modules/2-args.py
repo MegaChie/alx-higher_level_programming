85764d700658ec9e9b764f34d04d144dd439f9a3
MegaChie <workspace.mega@gmail.com>
2023-01-05 12:18:14 +0200

    Create 2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+	import sys
+	argumentVector = sys.argv[1:]
+	argumentCount = len(argumentVector)
+	count = 1
+	if argumentCount == 0:
+		print(f"{argumentCount} arguments.")
+	elif argumentCount == 1:
+		print(f"{argumentCount} argument:")
+		print(f"{index}: {sys.argv[1]}")
+	elif
+	print(f"{argumentCount} arguments:")
+	while index <= argumentCount:
+		print(f"{index}: {sys.argv[index]}")
+		index = index +1
