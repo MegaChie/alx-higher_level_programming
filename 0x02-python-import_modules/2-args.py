3c37489a269b3a049a3b38fdbf3cb17f9d91b612
MegaChie <workspace.mega@gmail.com>
2023-01-05 13:02:48 +0200

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-	import sys
-	argumentVector = sys.argv[1:]
-	argumentCount = len(argumentVector)
-	count = 1
-	if argumentCount == 0:
-		print("{} arguments.".format(argumentCount))
-	elif argumentCount == 1:
-		print("{} argument:".format(argumentCount))
-		print("{}: {}".format(count, sys.argv[1]))
-	elif argumentCount > 1:
-		print("{} arguments:".format(argumentCount))
-		while count <= argumentCount:
-			print("{}: {}".format(count, sys.argv[count]))
-			count = count +1
+    import sys
+    argumentVector = sys.argv[1:]
+    argumentCount = len(argumentVector)
+    count = 1
+    if argumentCount == 0:
+        print("{} arguments.".format(argumentCount))
+    elif argumentCount == 1:
+        print("{} argument:".format(argumentCount))
+        print("{}: {}".format(count, sys.argv[1]))
+    elif argumentCount > 1:
+        print("{} arguments:".format(argumentCount))
+        while count <= argumentCount:
+            print("{}: {}".format(count, sys.argv[count]))
+            count = count + 1
