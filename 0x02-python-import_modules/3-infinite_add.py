88a85115e27bc7df7cca0407a6c4706bd6f1971f
MegaChie <workspace.mega@gmail.com>
2023-01-05 18:11:08 +0200

    Update 3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-import sys
-argumentVector = sys.argv[1:]
-argumentCount = len(argumentVector)
-count = 1
-result = 0
-while count <= argumentCount:
-	result += int(sys.argv[index])
-	count += 1
-print("{}".format(result))
+if __name__ == "__main__":
+	import sys
+	argumentVector = sys.argv[1:]
+	argumentCount = len(argumentVector)
+	count = 1
+	result = 0
+	while count <= argumentCount:
+		result += int(sys.argv[index])
+		count += 1
+	print("{}".format(result))
