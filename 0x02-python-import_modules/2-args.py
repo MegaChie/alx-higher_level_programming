5665af0d7cadecb9b8dda58266e9dec43d4a1f4a
MegaChie <workspace.mega@gmail.com>
2023-01-05 12:20:18 +0200

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -9,8 +9,8 @@ if __name__ == "__main__":
 	elif argumentCount == 1:
 		print(f"{argumentCount} argument:")
 		print(f"{index}: {sys.argv[1]}")
-	elif
-	print(f"{argumentCount} arguments:")
-	while index <= argumentCount:
-		print(f"{index}: {sys.argv[index]}")
-		index = index +1
+	elif argumentCount > 1:
+		print(f"{argumentCount} arguments:")
+		while index <= argumentCount:
+			print(f"{index}: {sys.argv[index]}")
+			index = index +1
