ca4f47a9b797d422baf8d7dd98c7f587ef9be991
MegaChie <workspace.mega@gmail.com>
2023-01-05 19:14:42 +0200

    Create 100-my_calculator.py

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+  import sys
+  from calculator_1 import sub, add, div, mul
+  agumentVector = sys.argv[1:]
+  argumentCount = len(argumentCount)
+  operator = ["+", "-", "*", "/"]
+  if argumentCount != 3:
+    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+    exit(1)
+  elif sys.argv[2] not in operator:
+    print("Unknown operator. Available operators: +, -, * and /")
+    exit(1)
+  else:
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    if sys.argv[2] == "+":
+      print("{} {} {} = {}".format(a, sys.argv[2], b, add(a, b)))
+    elif sys.argv[2] == "*":
+      print("{} {} {} = {}".format(a, sys.argv[2], b, mul(a, b)))
+    elif sys.argv[2] == "-":
+      print("{} {} {} = {}".format(a, sys.argv[2], b, sub(a, b)))
+    elif sys.argv[2] == "/":
+      print("{} {} {} = {}".format(a, sys.argv[2], b, div(a, b)))
