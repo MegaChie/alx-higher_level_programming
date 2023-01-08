3c77008b83e90f95fd954386f8728897a032b9fd
MegaChie <workspace.mega@gmail.com>
2023-01-08 17:10:00 +0200

    Update 4-new_in_list.py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,7 +2,7 @@
 def new_in_list(my_list, idx, element):
     if idx > 0:
         return my_list
-    elif >= len(my_list):
+    elif <= len(my_list):
         return my_list
     listCopy = list(my_list)
     listCopy[idx] = element
