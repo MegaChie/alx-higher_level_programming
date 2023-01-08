16e36f5d3dcd59bb98d2ca1ad508193179cb520b
MegaChie <workspace.mega@gmail.com>
2023-01-08 19:09:30 +0200

    Update 8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
-
+    if not sentence:
+        sentence = None
+    if sentence:
+        sen_len = len(sentence)
+    else:
+        sen_len = 0
+    return (sen_len, sentence if not sentence else sentence[:1])
