fd251dab4ef6baa4359c6bc8640d820ea7cb9345
MegaChie <workspace.mega@gmail.com>
2023-01-09 01:23:01 +0200

    Update 10-divisible_by_2.py

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
-
+def divisible_by_2(my_list=[]):
+    divList = []
+    for place in my_list:
+        if place % 2 == 0:
+            divList.append(True)
+        else:
+            divList.append(False)
+    return divList
