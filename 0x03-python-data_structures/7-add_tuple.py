5c8d70fa80a98de48d86913751e3309d7d3f9d11
MegaChie <workspace.mega@gmail.com>
2023-01-08 18:09:52 +0200

    Update 7-add_tuple.py

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -6,10 +6,10 @@
     elif len(tuple_a) == 0:
         tuple_a += (0,0)
     if len(tuple_b) > 2:
-        tuple_a = tuple_a[:2]
+        tuple_b = tuple_a[:2]
     elif len(tuple_b) == 1:
-        tuple_a += (0,)
+        tuple_b += (0,)
     elif len(tuple_b) == 0:
-        tuple_a += (0,0)
-    result = tuple(map(sum, zip(test_tup1, test_tup2)))
+        tuple_b += (0,0)
+    result = tuple(map(lambda i, j: i + j, tuple_a, tuple_b))
     return result
