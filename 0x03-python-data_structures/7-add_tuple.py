a8fe82fe2676ae565ff8da4b0be6b626adf7413f
MegaChie <workspace.mega@gmail.com>
2023-01-08 17:29:55 +0200

    Create 7-add_tuple.py

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+    if len(tuple_a) > 2:
+        tuple_a = tuple_a[:2]
+    elif len(tuple_a) == 1:
+        tuple_a += (0,)
+    elif len(tuple_a) == 0:
+        tuple_a += (0,0)
+    if len(tuple_b) > 2:
+        tuple_a = tuple_a[:2]
+    elif len(tuple_b) == 1:
+        tuple_a += (0,)
+    elif len(tuple_b) == 0:
+        tuple_a += (0,0)
+    result = tuple(map(lambda i, j: i + j, tuple_a, tuple_b))
+    return result
