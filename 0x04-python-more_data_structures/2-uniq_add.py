319b72d1a1d71ed54e0f7c32dbb5efbd3e1d1de8
MegaChie <workspace.mega@gmail.com>
2023-01-12 00:05:35 +0200

    Create 2-uniq_add.py

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    backup = list()
+    addup = 0
+    for place in my_list:
+        if place not in backup:
+            backup.append(place)
+        else:
+            continue
+    for holder in backup:
+        addup += holder
+    return addup 
