86863bb7dc3f049d1da3273381fb67d263f7ce28
MegaChie <workspace.mega@gmail.com>
2023-01-12 02:46:12 +0200

    Update 0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    mulMatrix = list(matrix)
-    if not matrix:
+    if len(matrix) == 0:
         print()
-    for raw in range(len(matrix)):
-            for collumn in range(len(matrix[raw])):
-                mulMatrix[raw][collumn] = matrix[raw][collumn] ** 2
-    return mulMatrix
+    return [[item**2 for item in row] for row in matrix]
