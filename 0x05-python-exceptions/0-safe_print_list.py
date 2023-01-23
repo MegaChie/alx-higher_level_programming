e0936c0aee9dc3ae74c9c67a862fc796e6868a11
MegaChie <workspace.mega@gmail.com>
2023-01-24 00:06:58 +0200

    Create 0-safe_print_list.py

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+    try:
+        count = 0
+        while count < x:
+            print("{}".format(my_list[count]), end='')
+            count += 1
+        print()
+        print("nb_print: {}".format(count))
+        return count
+    except IndexError:
+        print()
+        print("nb_print: {}".format(count))
