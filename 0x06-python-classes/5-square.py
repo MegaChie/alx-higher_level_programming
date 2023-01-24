263edd9564d46ea17cb6a8d2d34a4e55ab99c159
MegaChie <workspace.mega@gmail.com>
2023-01-24 21:20:14 +0200

    Create 5-square.py

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+class Square:
+    def __init__(self, size=0):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+    def area(self):
+        area = self.__size
+        return area * area
+    def size(self):
+        return self.__size
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+    def my_print(self):
+        printer = self.__size
+        if printer == 0:
+            print()
+        else:
+            for printed in range(printer):
+                print('#' * printed)
