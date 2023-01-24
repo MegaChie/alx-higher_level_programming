61be2ae37068dcc508170dec06963f46a4f0a374
MegaChie <workspace.mega@gmail.com>
2023-01-24 23:39:12 +0200

    Update 3-square.py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+""" Defines a square """
+
+
 class Square:
+    """ Area of a square
+    args: size - of integer value"""
     def __init__(self, size=0):
         if type(size) != int:
             raise TypeError("size must be an integer")
@@ -7,6 +12,9 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self.__size = size
+
+    """ Area of a square
+    args: size - of integer value"""
     def area(self):
         area = self.__size
         return area * area
