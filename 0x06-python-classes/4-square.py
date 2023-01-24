2b8d7bcdbbafcc4f00d214967338391530128b2a
MegaChie <workspace.mega@gmail.com>
2023-01-24 23:50:05 +0200

    Update 4-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -6,12 +6,10 @@ class Square:
     """ Access and update private attribute
     args: size - of integer value and is bigger or equal to 0"""
     def __init__(self, size=0):
-        if type(size) != int:
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
 
     """ Access and update private attribute """
     def area(self):
