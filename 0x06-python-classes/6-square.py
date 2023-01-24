cc61bf3b2d846c8455d300285da17c2ddd658c52
MegaChie <workspace.mega@gmail.com>
2023-01-25 00:21:51 +0200

    Update 6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+""" Defines a square """
+
+
 class Square:
+    """ Access and update private attribute
+    args: size - of integer value and is bigger or equal to 0 """
     def __init__(self, size=0):
         if type(size) is not int:
             raise TypeError("size must be an integer")
@@ -7,11 +12,18 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self.__size = size
+
+    """ Access and update private attribute """
     def area(self):
         area = self.__size
         return area * area
+
+    """ Access and update private attribute """
     def size(self):
         return self.__size
+
+    """ Access and update private attribute
+    args: value - of integer value and is bigger or equal to 0 """
     def size(self, value):
         if type(value) is not int:
             raise TypeError("size must be an integer")
@@ -19,6 +31,8 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self.__size = value
+
+    """ Coordinates of a square """
     def my_print(self):
         size = self.__size
         nl = self.__position[1]
@@ -29,8 +43,13 @@ class Square:
             print()
         for row in range(size):
             print((' ' * ws) + ('#' * size))
+
+    """ Coordinates of a square """
     def position(self):
         return self.__position
+
+    """ Coordinates of a square
+    args: value - of integer value and is tuple and bigger than 0 """
     def position(self, value):
         text = 'position must be a tuple of 2 positive integers'
         if type(value) is not tuple or len(value) != 2:
@@ -39,6 +58,10 @@ class Square:
             if type(place) is not int or place < 0:
                 raise TypeError(text)
         self.__position = value
+        
+    """ Coordinates of a square
+    args: value - of integer value and is tuple and bigger than 0
+          position - tuple representing x and y coordinates """
     def __init__(self, size=0, position=(0, 0)):
         self.size = size
         self.position = position
