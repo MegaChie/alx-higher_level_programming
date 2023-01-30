96bfa799d7ce53434af06ea5dcf9d68523c42b88
MegaChie <workspace.mega@gmail.com>
2023-01-30 16:29:20 +0200

    Update 1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -4,11 +4,13 @@
 
 class Rectangle:
     """  Real definition of a rectangle. """
+    @property
     def width(self):
         return self.__width
 
     """ Real definition of a rectangle.
     args: value - of integer  type """
+    @width.setter
     def width(self, value):
         if type(value) is not int:
             raise TypeError("width must be an integer")
@@ -17,10 +19,12 @@ class Rectangle:
         self.__width = value
     
     """ Real definition of a rectangle. """
+    @property
     def height(self):
         return self.__height
 
     """ Real definition of a rectangle. """
+    @height.setter
     def height(self, value):
         if type(value) is not int:
             raise TypeError("width must be an integer")
