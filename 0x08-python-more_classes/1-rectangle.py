142b3ebd896afbfb2e306025cb14cac4cbbaa5d7
MegaChie <workspace.mega@gmail.com>
2023-01-30 16:19:52 +0200

    Create 1-rectangle.py

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+""" Rectangle class  """
+
+
+class Rectangle:
+    """  Real definition of a rectangle. """
+    def width(self):
+        return self.__width
+
+    """ Real definition of a rectangle.
+    args: value - of integer  type """
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+    
+    """ Real definition of a rectangle. """
+    def height(self):
+        return self.__height
+
+    """ Real definition of a rectangle. """
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__height = value
+
+    """ Real definition of a rectangle.
+    Args: width - of integer type
+          height - of integer type """
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
