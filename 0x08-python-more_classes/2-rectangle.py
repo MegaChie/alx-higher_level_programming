8006035daa164a870746167e3efc1aaa43d81e01
MegaChie <workspace.mega@gmail.com>
2023-01-30 17:00:47 +0200

    Update 2-rectangle.py

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -3,12 +3,12 @@
 
 
 class Rectangle:
-    """  Real definition of a rectangle. """
+    """  Area and Perimeter. """
     @property
     def width(self):
         return self.__width
 
-    """ Real definition of a rectangle.
+    """ Area and Perimeter.
     args: value - of integer  type """
     @width.setter
     def width(self, value):
@@ -18,12 +18,12 @@ class Rectangle:
             raise ValueError("width must be >= 0")
         self.__width = value
 
-    """ Real definition of a rectangle. """
+    """ Area and Perimeter. """
     @property
     def height(self):
         return self.__height
 
-    """ Real definition of a rectangle. """
+    """ Area and Perimeter. """
     @height.setter
     def height(self, value):
         if type(value) is not int:
@@ -32,18 +32,18 @@ class Rectangle:
             raise ValueError("height must be >= 0")
         self.__height = value
 
-    """ Real definition of a rectangle.
+    """ Area and Perimeter.
     Args: width - of integer type
           height - of integer type """
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
 
-    """ Real definition of a rectangle. """
+    """ Area and Perimeter. """
     def area(self):
         return self.__height * self.__width
 
-    """ Real definition of a rectangle. """
+    """ Area and Perimeter. """
     def perimeter(self):
         if self.__height == 0 or self.__width == 0:
             return 0
