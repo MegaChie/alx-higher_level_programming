46d498f8f075fddd9b860dab5bc9462040884a00
MegaChie <workspace.mega@gmail.com>
2023-01-30 16:43:24 +0200

    Update 1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,40 +1,48 @@
 #!/usr/bin/python3
-""" Rectangle class  """
+"""
+A rectangle with width and height.
+"""
 
 
 class Rectangle:
-    """  Real definition of a rectangle. """
+    """
+    Rectangle functions and data
+    """
+
+    def __init__(self, width=0, height=0):
+        """ Instantiation
+        """
+        self.width = width
+        self.height = height
+
     @property
     def width(self):
+        """ Getter for width
+        """
         return self.__width
 
-    """ Real definition of a rectangle.
-    args: value - of integer  type """
     @width.setter
     def width(self, value):
-        if type(value) is not int:
+        """ Setter for width
+        """
+        if type(value) != int:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
-    
-    """ Real definition of a rectangle. """
+
     @property
     def height(self):
+        """ Getter for height
+        """
         return self.__height
 
-    """ Real definition of a rectangle. """
     @height.setter
     def height(self, value):
-        if type(value) is not int:
+        """ Setter for height
+        """
+        if type(value) != int:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
-
-    """ Real definition of a rectangle.
-    Args: width - of integer type
-          height - of integer type """
-    def __init__(self, width=0, height=0):
-        self.width = width
-        self.height = height
