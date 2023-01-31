75173cd27544f872ca80e0b53a63943d5c6e2ccc
MegaChie <workspace.mega@gmail.com>
2023-01-31 23:06:40 +0200

    Create 101-lazy_matrix_mul.py

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3.5
+""" multiply matrix by matrix. """
+
+
+import numpy as np
+def lazy_matrix_mul(m_a, m_b):
+    """ Function that multiplies 2 matrices.
+    Args: m_a - matrix a
+          m_b - matrix b """
+
+    return (np.matmul(m_a, m_b))
