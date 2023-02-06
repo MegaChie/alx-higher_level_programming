1b786007883b26029b52e4eb513face485fc825d
MegaChie <workspace.mega@gmail.com>
2023-02-07 00:46:32 +0200

    Update 9-rectangle.py

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -4,17 +4,17 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """ Full rectangle """
+    """ Full rectangle. """
     def __init__(self, width, height):
         self.__height = height
         self.__width = width
         super().integer_validator("width", width)
         super().integer_validator("height", height)
 
-    """ Full rectangle """
+    """ Full rectangle. """
     def area(self):
         return self.__height * self.__width
 
-    """ Full rectangle """
+    """ Full rectangle. """
     def __str__(self):
         return "[Rectangle] {}/{}".format(self.__width, self.__height)
