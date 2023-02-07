774aef2f33734863a9648009abe50935484db652
MegaChie <workspace.mega@gmail.com>
2023-02-07 18:32:19 +0200

    Update 0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,6 +4,6 @@
 
 def read_file(filename=""):
     """ Read file """
-    with open(filename, encoding="utf-8") as f
-    reader = f.read()
-    print(reader, end="")
+    with open(filename, encoding="utf-8") as f:
+        reader = f.read()
+        print(reader, end="")
