803f7c370ef7525ae3e69fffe6179e487920e0e4
MegaChie <workspace.mega@gmail.com>
2023-02-08 00:25:53 +0200

    Update 10-student.py

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -11,7 +11,7 @@ class Student:
 
     def to_json(self, attrs=None):
         """ Student to JSON """
-        if attrs == None or type(attrs) != list:
+        if attrs is None or type(attrs) != list:
             return self.__dict__
         else:
             temp = {}
