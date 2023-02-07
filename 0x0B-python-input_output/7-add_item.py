4092b34ae7af0ea25f68d674971ad0e45a87ee13
MegaChie <workspace.mega@gmail.com>
2023-02-07 23:53:52 +0200

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -8,8 +8,8 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 try:
     loaded = load_from_json_file("add_item.json")
 except FileNotFoundError:
-    fileList = []
+    loaded = []
 arrgumentCount = len(sys.argv)
 for place in range(1, arrgumentCount):
-    fileList.append(sys.argv[place])
-save_to_json_file(fileList, "add_item.json")
+    loaded.append(sys.argv[place])
+save_to_json_file(loaded, "add_item.json")
