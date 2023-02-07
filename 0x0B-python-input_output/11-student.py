f2ae39eb5124ac355d26a9f04d5afabbcd04a0f0
MegaChie <workspace.mega@gmail.com>
2023-02-08 00:48:18 +0200

    Create 11-student.py

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" user definned class. """
+
+
+class Student:
+    """ Student to disk and reload """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ Student to disk and reload """
+        if attrs is None or type(attrs) != list:
+            return self.__dict__
+        else:
+            temp = {}
+            for place in attrs:
+                if type(place) != str:
+                    return self.__dict__
+                if place in self.__dict__.keys():
+                    temp[place] = self.__dict__[place]
+            return temp
+
+    def reload_from_json(self, json):
+        """ Student to disk and reload """
+        self.__dict__[items] = json[items]
