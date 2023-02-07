14f51f2f7b8f2fa2f4d665c11b2cb9f2da69171f
MegaChie <workspace.mega@gmail.com>
2023-02-07 18:07:06 +0200

    Create 0-read_file.py

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" user definned function. """
+
+
+def read_file(filename=""):
+    """ Read file """
+    with open(filename, encoding="utf-8") as f
+    reader = f.read()
+    print(reader, end="")
