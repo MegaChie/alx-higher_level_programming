263766bfb4f6b8b8fdf6c91f0abd72d566aba9d4
MegaChie <workspace.mega@gmail.com>
2023-02-11 21:12:43 +0200

    Create rectangle.py

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,90 @@
+#!/usr/bin/python3
+""" 0x0C. Python - Almost a circle """
+
+
+class Rectangle(Base):
+    """ First Rectangle """
+    def __init__(self, width, height, x=0, y=0, id=None):
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+    
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if  value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+    
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if  value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
+    
+    @property
+    def x(self):
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
+    
+    @property
+    def y(self):
+        return self.__x
+
+    @y.setter
+    def y(self, value):
+        if type(value) is not int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
+
+    def area(self):
+        """ Area first """
+        return self.width * self.__height
+
+    def display(self):
+        """ Display #0 """
+        if self.__y != 0:
+            for lines in range(self.__y):
+                print()
+        for lines in range(self.__height):
+            print((self.__x * " ") + ('#' * self.__width))
+
+    def __str__(self):
+        """ __str__ """
+        return "[{}] ({}) {}/{} - {}/{}".format(self.__class__.__name__,
+                                                self.id, self.__x, self.__y,
+                                                self.__width, self.__height)
+        
+    def update(self, *args):
+        """ Update #0 """
+        if len(args) != 0:
+            try:
+                self.id = args[0]
+                self.__width = args[1]
+                self.__height = args[2] 
+                self.__x = args[3]
+                self.__y = args[4]
+            except IndexError:
+                pass
