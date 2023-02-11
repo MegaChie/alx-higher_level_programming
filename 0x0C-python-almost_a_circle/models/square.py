c2ac5e8d7bd27f00b8df411a7430fd3ddefe1c6b
MegaChie <workspace.mega@gmail.com>
2023-02-11 23:36:13 +0200

    Create square.py

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" 0x0C. Python - Almost a circle """
+
+
+class Square(Rectangle):
+    """ And now, the Square! """
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(width=size, height=size, x=x, y=y, id=id)
+
+    # def size(self):
+    #     return self.width
+
+    # @size.setter
+    # def size(self, value):
+    #     self.width = value
+    #     self.height = value
+
+    def __str__(self):
+        return "[{}] ({}) {}/{} - {}".format(self.__class__.__name__,
+                                             self.id, self.x, self.y,
+                                             self.width)
