6d25ac50adda0be30171da134776ae7138be7956
MegaChie <workspace.mega@gmail.com>
2023-02-13 17:55:11 +0200

    Update base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -6,8 +6,8 @@ import json
 
 
 class Base:
-    __nb_objects = 0
     """ Base class """
+    __nb_objects = 0
     def __init__(self, id=None):
         if id is not None:
             self.id = id
