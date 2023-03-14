20df2fd923c468aff99d4a343cec0c5180041479
MegaChie <workspace.mega@gmail.com>
2023-03-15 00:05:50 +0200

    Update 4-first_table.sql

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,4 +1,11 @@
 -- comment text
-CREATE TABLE first_table (
+CREATE TABLE second_table (
 id INT,
-name VARCHAR(256));
+name VARCHAR(256),
+score INT);
+INSERT INTO second_table
+VALUES
+(1, 'John', 10),
+(2, 'Alex', 3),
+(3, 'Bob', 14),
+(4, 'George', 8);
