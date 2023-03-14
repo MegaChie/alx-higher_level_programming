a715f25ff2b144b3603c0c410ee4da7203300c52
MegaChie <workspace.mega@gmail.com>
2023-03-15 00:10:29 +0200

    Update 4-first_table.sql

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,11 +1,2 @@
--- comment text
-CREATE TABLE second_table (
-id INT,
-name VARCHAR(256),
-score INT);
-INSERT INTO second_table
-VALUES
-(1, 'John', 10),
-(2, 'Alex', 3),
-(3, 'Bob', 14),
-(4, 'George', 8);
+-- creates a table called first_table
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
