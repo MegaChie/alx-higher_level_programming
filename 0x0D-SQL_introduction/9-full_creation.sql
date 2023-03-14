47d5e8e4481be54c222551952ad06d720164ba2f
MegaChie <workspace.mega@gmail.com>
2023-03-15 00:12:55 +0200

    Update 9-full_creation.sql

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,9 +1,9 @@
 -- comment text
-CREATE TABLE second_table (
+CREATE TABLE IF NOT EXISTS second_table (
 id INT,
 name VARCHAR(256),
 score INT);
-INSERT INTO second_table(id, name, score)
+INSERT INTO second_table
 VALUES
 (1, 'John', 10),
 (2, 'Alex', 3),
