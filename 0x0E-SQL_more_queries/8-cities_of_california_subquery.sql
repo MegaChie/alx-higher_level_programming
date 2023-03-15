a30c43754dbd45807be200aeb38a8ce9eca64edc
MegaChie <workspace.mega@gmail.com>
2023-03-16 00:46:48 +0200

    Update 8-cities_of_california_subquery.sql

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,6 @@
 -- comment text
-SELECT id, name FROM *
-WHERE nmae = 'California'
+SELECT cities.id, cities.name
+FROM cities, states
+WHERE cities.state_id = states.id
+    AND states.name = 'California'
 ORDER BY cities.id;
