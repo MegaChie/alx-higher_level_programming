089600a58bbeba260569d36472bb629b65742c79
MegaChie <workspace.mega@gmail.com>
2023-03-15 02:02:06 +0200

    Update 100-move_to_utf8.sql

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,2 +1,3 @@
 -- comment text
-alter table first_table change name name VARCHAR(256) character set utf8;
+USE `hbtn_0c_0`
+alter table first_table change name name VARCHAR(256) character setutf8mb4 COLLATE utf8mb4_unicode_ci;
