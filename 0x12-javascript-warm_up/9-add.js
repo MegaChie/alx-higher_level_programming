a12ec4240de324a66337adb7daae9631db117aaa
MegaChie <workspace.mega@gmail.com>
2023-04-11 15:10:37 +0200

    Update 9-add.js

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,5 +1,7 @@
 #!/usr/bin/node
 // comment text
+const a = process.argv[2];
+const b = process.argv[3]
 function add(a, b) {
-  return (console.log(process.argv[2] + process.argv[3]));
+  console.log(process.argv[2] + process.argv[3]);
 }
