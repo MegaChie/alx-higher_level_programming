75e8fc71396f9a99c448519e32745f69630a55da
MegaChie <workspace.mega@gmail.com>
2023-04-11 12:17:57 +0200

    Revert "Update 3-value_argument.js"
    This reverts commit f1b2e59d541066d3c6215dea29522997c5c9129e.

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 // comment text
 if (process.argv.length === 2) {
-  console.log('No argument')
+  console.log('No argument');
 } else {
   for (let i = 2; i < process.argv.length; i++) {
-    console.log(process.argv[i])
+    console.log(process.argv[i]);
   }
 }
