ffd2e3282569fe819779971d2f1af9d5b2bfccfa
MegaChie <workspace.mega@gmail.com>
2023-04-11 12:49:15 +0200

    Update 5-to_integer.js

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // comment text
 let holder = process.argv[2];
-if (typeof(holder) == number) {
+if (typeof(holder) == 'number') {
   console.log(holder);
 } else {
   console.log('Not a number');
