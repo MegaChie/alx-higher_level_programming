68102f764bbb26794d63fba8af44e54a59dd3fbf
MegaChie <workspace.mega@gmail.com>
2023-04-12 01:51:22 +0200

    Update 11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,15 +1,16 @@
 #!/usr/bin/node
 // comment text
-let arr = [0]
 function sortNumber (a, b) {
   return a - b;
 }
-if (process.argv.length == 2 || process.argv.length == 3) {
+let argsLen = process.argv.length;
+if (argsLen === 2 || argsLen === 3) {
   console.log('0');
 } else {
+  let arr = [];
   for (let i = 2; i < argsLen; i++) {
     arr.push(process.argv[i]);
   }
   arr.sort(sortNumber);
-  console.log(arr.slice(-2));
+  console.log(arr[arr.length - 2]);
 }
