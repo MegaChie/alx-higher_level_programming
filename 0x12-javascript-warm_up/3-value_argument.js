f6e4502e45bd2d28950629086daf03c1b024e909
MegaChie <workspace.mega@gmail.com>
2023-04-11 11:57:38 +0200

    Update 3-value_argument.js

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -2,8 +2,6 @@
 // comment text
 if (process.argv.length === 2) {
   console.log('No argument');
-} else if (process.argv.length === 3) {
-  console.log('Argument found');
 } else {
-  console.log('Arguments found');
+  console.log(process.argv);
 }
