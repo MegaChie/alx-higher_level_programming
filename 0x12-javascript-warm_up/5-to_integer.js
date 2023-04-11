eb2dad1e973286a30e47900218de8259b8360c9a
MegaChie <workspace.mega@gmail.com>
2023-04-11 13:05:38 +0200

    Update 5-to_integer.js

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,6 +2,6 @@
 // comment text
 if (isNaN(process.argv[2]) == 'true') {
   console.log('Not a number');
-} else if (isNaN(process.argv[2]) == 'false') {
+} else (isNaN(process.argv[2]) == 'false') {
   console.log('My number: ' + process.argv[2]);
 }
