8e4e8b89b41e29129f0a938b44ad86cd535f2aab
MegaChie <workspace.mega@gmail.com>
2023-04-11 12:31:40 +0200

    Update 4-concat.js

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,9 +1,3 @@
 #!/usr/bin/node
 // comment text
-if (process.argv.length === 2) {
-  console.log('No argument');
-} else {
-  for (let i = 2; i < process.argv.length; i++) {
-    console.log(process.argv[i]);
-  }
-}
+console.log(process.argv[2] + 'is' + process.argv[3]);
