0ea917f588922821414ca8a513050275f0afdd5f
MegaChie <workspace.mega@gmail.com>
2023-04-11 14:19:41 +0200

    Update 6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 // comment text
-const list = ["C is fun", "Python is cool", "JavaScript is amazing"];
+const list = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let i = list.length - 1; i >= 0; i--) {
   console.log(list[i]);
 }
