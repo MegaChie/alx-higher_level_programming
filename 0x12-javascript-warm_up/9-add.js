e54048a99ba47bd5d915009aa666a2e1458f2184
MegaChie <workspace.mega@gmail.com>
2023-04-11 15:07:21 +0200

    Update 9-add.js

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,13 +1,5 @@
 #!/usr/bin/node
 // comment text
-let num = Number(process.argv[2]);
-
-if (num) {
-  for (let i = 0; i < num; i++) {
-    console.log('X'.repeat(num));
-  }
-} else if (num < 0) {
-  console.log('');
-} else {
-  console.log('Missing size');
+function add(a, b) {
+  console.log(process.argv[2] + process.argv[3]);
 }
