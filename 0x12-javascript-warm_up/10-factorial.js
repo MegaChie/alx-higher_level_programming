debccf0a084f4cc9007b60ba3bd48c4366f2e475
MegaChie <workspace.mega@gmail.com>
2023-04-12 00:47:53 +0200

    Update 10-factorial.js

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 // comment text
 function factorial (x) {
-  if (x == 0) {
-        console.log('1');
-    }
-    else {
-        console.log(x * factorial(x - 1));
-    }
+  if (isNaN(x) === true || x <= 0) {
+    console.log('1');
+  } else {
+    let temp = x * factorial(x - 1);
+    console.log(temp);
+  }
 }
-let pass = Number(process.argv[2]);
+const pass = Number(process.argv[2]);
 factorial(pass);
