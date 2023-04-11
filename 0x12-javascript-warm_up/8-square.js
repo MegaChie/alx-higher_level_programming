9c82b65762ecdf3b4320c91ced6b8ddf45802540
MegaChie <workspace.mega@gmail.com>
2023-04-11 14:42:29 +0200

    Update 8-square.js

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -4,6 +4,6 @@ if (isNaN(process.argv[2]) === true) {
   console.log('Missing size');
 } else {
   for (let i = 0; i < Number(process.argv[2]); i++) {
-    console.log('x' * Number(process.argv[2]));
+    console.log("x".repeat(Number(process.argv[2])));
   }
 }
