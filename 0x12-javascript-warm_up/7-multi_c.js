c87595a07d1316384649b2d47c68f249492a6580
MegaChie <workspace.mega@gmail.com>
2023-04-11 14:32:26 +0200

    Update 7-multi_c.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // comment text
 if (isNaN(process.argv[2]) === true) {
-  console.log('Missing number of occurrences')
+  console.log('Missing number of occurrences');
 } else {
   for (let i = 0; i < Number(process.argv[2]); i++) {
     console.log('C is fun');
