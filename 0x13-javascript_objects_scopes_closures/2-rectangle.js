dff15c6876d38c3543195a8b131721bb269648dc
MegaChie <workspace.mega@gmail.com>
2023-04-12 14:51:46 +0200

    Update 2-rectangle.js

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,7 +2,9 @@
 // comment text
 module.exports = class Rectangle {
   constructor (w, h) {
-    this.height = h;
-    this.width = w;
+    if (w > 0 || h > 0) {
+      this.height = h;
+      this.width = w;
+  }
   }
 };
