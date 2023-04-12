c05c515e3cf787abe06efa7212a6c03f73677bff
MegaChie <workspace.mega@gmail.com>
2023-04-12 23:50:37 +0200

    Update 9-logme.js

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
 // comment text
-exports.esrever = function (list) {
-  const used = [];
-  for (let i = list.length - 1; i >= 0; i--) {
-    used.push(list[i]);
-  }
-  return used;
+exports.logMe = function (item) {
+  let n = 0;
+  console.log(n : ':' + item);
+  n++;
 };
