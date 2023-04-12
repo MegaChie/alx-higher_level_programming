eab5163c2029d9f61d2b4cedeac9de35cfbaa343
MegaChie <workspace.mega@gmail.com>
2023-04-12 23:26:52 +0200

    Update 8-esrever.js

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // comment text
 exports.esrever = function (list) {
-  let used = [];
+  const used = [];
   for (let i = list.length - 1; i >= 0; i--) {
     used.push(list[i]);
   }
