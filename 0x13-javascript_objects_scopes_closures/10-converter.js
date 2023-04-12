bf7ce1c5a47a521b31be7c5cd35971b04dc8440b
MegaChie <workspace.mega@gmail.com>
2023-04-13 00:29:14 +0200

    Update 10-converter.js

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,11 +1,7 @@
 #!/usr/bin/node
 // comment text
 exports.converter = function (base) {
-  if (process.argv[2] === '16') {
-    console.log(base.toString(16));
-  } else if (process.argv[2] === '2') {
-    console.log(base.toString(2));
-  } else if (process.argv[2] === '8') {
-    console.log(base.toString(8));
-  }
+  return function mainConvert (num) {
+    return num.toString(base);
+  };
 };
