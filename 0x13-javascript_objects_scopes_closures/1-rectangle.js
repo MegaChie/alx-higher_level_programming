bf9b06de2ae59ddcbda432da932d323f139668d5
MegaChie <workspace.mega@gmail.com>
2023-04-12 14:34:09 +0200

    Update 1-rectangle.js

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,3 +1,9 @@
 #!/usr/bin/node
 // comment text
-module.exports = class Rectangle {};
+module.exports = class Rectangle {
+  width , height;
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+};
