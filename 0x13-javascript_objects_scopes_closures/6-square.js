dae76f7bbdc9dd6478ff6e34c6db7c652d1e6935
MegaChie <workspace.mega@gmail.com>
2023-04-12 23:00:42 +0200

    Update 6-square.js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
 // comment text
-module.exports = class Square extends require('./5-square') {
-  charPrint (c) {
-    if (c === undefined) {
-      c = 'X';
-    }
-    for (let i = 0; i < this.height; i++) {
-      console.log(c.repeat(this.width));
+exports.nbOccurences = function (list, searchElement) {
+  let n = 0;
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] === searchElement) {
+      n++;
     }
   }
+  return n;
 };
