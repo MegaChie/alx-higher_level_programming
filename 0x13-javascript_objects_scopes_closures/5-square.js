cf652d0b8899486a2153c2399675f425c823d495
MegaChie <workspace.mega@gmail.com>
2023-04-12 15:44:21 +0200

    Update 5-square.js

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,27 +1,7 @@
 #!/usr/bin/node
 // comment text
-module.exports = class Rectangle {
-  constructor (w, h) {
-    if (w > 0 && h > 0) {
-      this.width = w;
-      this.height = h;
-    }
-  }
-
-  print () {
-    for (let i = 0; i < this.height; i++) {
-      console.log('x'.repeat(this.width));
-    }
-  }
-
-  rotate () {
-    let temp = this.height;
-    this.height = this.width;
-    this.width = temp;
-  }
-
-  double () {
-    this.width *= 2;
-    this.height *= 2;
+module.exports = class Square extends require('./4-rectangle') {
+  constructor (size) {
+    super(size, size);
   }
 };
