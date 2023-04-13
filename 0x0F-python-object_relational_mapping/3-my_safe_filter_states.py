2052ad136a01fbeea8badbb54def2de5d72aa2ef
MegaChie <workspace.mega@gmail.com>
2023-04-13 23:20:13 +0200

    Create 3-my_safe_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""  lists all states from the database hbtn_0e_0_usa. """
+import MySQLdb
+import sys
+
+
+def Fetch():
+    """ SQL Injection... """
+    if (len(sys.argv) == 5):
+        cofig = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                                passwd=sys.argv[2], db=sys.argv[3])
+        code = config.cursor()
+        code.execute("SELECT * FROM states WHERE BINARY name = '{:s}\
+            ORDER BY id ASC".format(sys.argv[4]))
+        file = code.fetchall()
+        for lines in file:
+            print(line)
+        code.close()
+        config.close()
+    else:
+        return
+
+if __name__ == "__main__":
+    get_states()
