b98eb4621a4db50acc68e1c56289995464ea97e2
MegaChie <workspace.mega@gmail.com>
2023-04-13 15:25:10 +0200

    Update 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,7 +7,7 @@ import sys
 def Fetch():
     """ Get all states. """
     cofig = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-        passwd=sys.argv[2], db=sys.argv[3])
+                            passwd=sys.argv[2], db=sys.argv[3])
     code = config.cursor()
     code.execute("SELECT * FROM states ORDER BY id ASC")
     file = code.fetchall()
