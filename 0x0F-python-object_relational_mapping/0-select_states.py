85ae18989413d49f611c419c965214b4c6c8aae9
MegaChie <workspace.mega@gmail.com>
2023-04-13 15:24:18 +0200

    Update 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,16 +5,16 @@ import sys
 
 
 def Fetch():
-	""" Get all states. """
-	cofig = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-		passwd=sys.argv[2], db=sys.argv[3])
-	code = config.cursor()
-	code.execute("SELECT * FROM states ORDER BY id ASC")
-	file = code.fetchall()
-	for lines in file:
-		print(line)
-	code.close()
-	config.close()
+    """ Get all states. """
+    cofig = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+        passwd=sys.argv[2], db=sys.argv[3])
+    code = config.cursor()
+    code.execute("SELECT * FROM states ORDER BY id ASC")
+    file = code.fetchall()
+    for lines in file:
+        print(line)
+    code.close()
+    config.close()
 
 if __name__ == "__main__":
-	get_states()
+    get_states()
