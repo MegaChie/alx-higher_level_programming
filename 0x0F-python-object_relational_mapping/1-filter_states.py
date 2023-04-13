581560eb24fda975db638e564e663cdcdb29c961
MegaChie <workspace.mega@gmail.com>
2023-04-13 15:36:29 +0200

    Update 1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -5,7 +5,7 @@ import sys
 
 
 def Fetch():
-    """ Get all states. """
+    """ Filter states. """
     cofig = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                             passwd=sys.argv[2], db=sys.argv[3])
     code = config.cursor()
