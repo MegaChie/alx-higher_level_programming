d9a68e532664817d8dc3a92a3615da483f538677
MegaChie <workspace.mega@gmail.com>
2023-04-14 23:17:59 +0200

    Update 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-"""Script that lists all states from database hbtn_0e_0_usa"""
+""" Script that lists all states from database hbtn_0e_0_usa """
 import MySQLdb
 import sys
 
 
 def get_states():
-    """ 0. Get all states """
+    """ 0. Get all states. """
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
 
