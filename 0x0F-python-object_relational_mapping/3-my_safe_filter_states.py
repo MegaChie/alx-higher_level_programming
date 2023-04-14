308a6280da177fb4abb2d5f299e4e32deea69650
MegaChie <workspace.mega@gmail.com>
2023-04-14 23:33:30 +0200

    Update 3-my_safe_filter_states.py

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -6,6 +6,7 @@ import sys
 
 
 def Fetch():
+    """ 3. SQL Injection... """
     if len(sys.argv) == 5:
         db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                              passwd=sys.argv[2], db=sys.argv[3])
