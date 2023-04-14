6d436bae711f33fb4728fc56f0b43702ced0acd2
MegaChie <workspace.mega@gmail.com>
2023-04-15 00:17:18 +0200

    Update 5-filter_cities.py

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -9,7 +9,7 @@ def Fetch():
     """ 5. All cities by state. """
     if len(sys.argv) == 5:
         db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                                passwd=sys.argv[2], db=sys.argv[3])
+                             passwd=sys.argv[2], db=sys.argv[3])
         cur = db.cursor()
         cur.execute("SELECT cities.name FROM cities\
                     JOIN states ON cities.state_id = states.id\
