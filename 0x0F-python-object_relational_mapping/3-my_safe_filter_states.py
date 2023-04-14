011bf50e02bd15adb12b4deb77bf38239eb9f433
MegaChie <workspace.mega@gmail.com>
2023-04-14 23:32:50 +0200

    Update 3-my_safe_filter_states.py

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python3
-"""  lists all states from the database hbtn_0e_0_usa. """
+""" a script that takes in an argument and displays all values in the states
+table of hbtn_0e_0_usa where name matches the argument. """
 import MySQLdb
 import sys
 
 
 def Fetch():
-    """ SQL Injection... """
     if len(sys.argv) == 5:
-        cofig = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                                passwd=sys.argv[2], db=sys.argv[3])
-        code = config.cursor()
-        code.execute("SELECT * FROM states WHERE BINARY name = '{:s}\
-            ORDER BY id ASC".format(sys.argv[4]))
-        file = code.fetchall()
+        db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                             passwd=sys.argv[2], db=sys.argv[3])
+        cur = db.cursor()
+        cur.execute("SELECT * FROM states WHERE BINARY name='{:s}'\
+                    ORDER BY id ASC".format(sys.argv[4]))
+        file = cur.fetchall()
         for lines in file:
-            print(line)
-        code.close()
-        config.close()
+            print(lines)
+        cur.close()
+        db.close()
     else:
         return
 
