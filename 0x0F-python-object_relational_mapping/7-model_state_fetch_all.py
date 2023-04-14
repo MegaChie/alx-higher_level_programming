9c7bfee68dc9185cefd73d288f22e25cac747d73
MegaChie <workspace.mega@gmail.com>
2023-04-15 00:39:22 +0200

    Update 7-model_state_fetch_all.py

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """ a script that lists all State objects from the database hbtn_0e_6_usa """
 import sys
-from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
+import sqlalchemy
 from model_state import Base, State
 
 
