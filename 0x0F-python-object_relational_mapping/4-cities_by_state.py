fb5c536644b06fe4433bf79ab3775f924078c383
MegaChie <workspace.mega@gmail.com>
2023-04-14 23:49:25 +0200

    Update 4-cities_by_state.py

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,23 +1,22 @@
 #!/usr/bin/python3
-"""  lists all states from the database hbtn_0e_0_usa. """
+""" a script that lists all cities from the database hbtn_0e_4_usa. """
 import MySQLdb
 import sys
 
 
 def Fetch():
-    """ Cities by states. """
-    cofig = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                            passwd=sys.argv[2], db=sys.argv[3])
-    code = config.cursor()
-    code.execute("SELECT cities.id, cities.name, states.name FROM cities\
+    """ 4. Cities by states. """
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT cities.id, cities.name, states.name FROM cities\
                 JOIN states ON cities.state_id = states.id\
                 ORDER BY cities.id ASC")
-    file = code.fetchall()
-    for lines in file:
+    file = cur.fetchall()
+    for line in file:
         print(line)
-    code.close()
-    config.close()
-
+    cur.close()
+    db.close()
 
 if __name__ == "__main__":
-    get_states()
+    Fetch()
