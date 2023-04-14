c32ab4e19f688409ff848a877c7260147b3305a3
MegaChie <workspace.mega@gmail.com>
2023-04-15 00:13:20 +0200

    Update 5-filter_cities.py

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-"""  lists all states from the database hbtn_0e_0_usa. """
+""" a script that takes in the name of a state as an argument and lists all
+cities of that state, using the database hbtn_0e_4_usa. """
 import MySQLdb
 import sys
 
 
 def Fetch():
-    """ All cities by state. """
+    """ 5. All cities by state. """
     if len(sys.argv) == 5:
         cofig = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                                 passwd=sys.argv[2], db=sys.argv[3])
@@ -27,4 +28,4 @@ def Fetch():
 
 
 if __name__ == "__main__":
-    get_states()
+    Fetch()
