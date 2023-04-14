2d9bb800712da5b61f5596d173af9aa497815894
MegaChie <workspace.mega@gmail.com>
2023-04-15 00:25:04 +0200

    Update 7-model_state_fetch_all.py

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -8,10 +8,10 @@ import sqlalchemy.orm
 
 def Fetch():
     """ 7. All states via SQLAlchemy. """
-	engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
-	Base.metadata.create_all(engine)
+    Base.metadata.create_all(engine)
     session = Session(engine)
     rows = session.query(State).all()
     for i in rows:
@@ -20,4 +20,4 @@ def Fetch():
 
 
 if __name__ == "__main__":
-	list_state_obj()
+    list_state_obj()
