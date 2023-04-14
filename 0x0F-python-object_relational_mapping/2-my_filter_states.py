ee2aee320317693bfe2fe3fa368089a505fad9f4
MegaChie <workspace.mega@gmail.com>
2023-04-14 23:26:16 +0200

    Update 2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,21 +1,24 @@
 #!/usr/bin/python3
-"""  lists all states from the database hbtn_0e_0_usa. """
+""" a script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument. """
 import MySQLdb
 import sys
 
 
-def Fetch():
-    """ Filter states by user input. """
-    cofig = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                            passwd=sys.argv[2], db=sys.argv[3])
-    code = config.cursor()
-    code.execute("SELECT * FROM states WHERE BINARY name = '{:s}\
-        ORDER BY id ASC".format(sys.argv[4]))
-    file = code.fetchall()
+def get_states():
+    """ 2. Filter states by user input. """
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM states WHERE BINARY name='{:s}'\
+                ORDER BY id ASC".format(sys.argv[4]))
+    file = cur.fetchall()
     for lines in file:
-        print(line)
-    code.close()
-    config.close()
+        print(lines)
+
+    cur.close()
+    db.close()
 
 
 if __name__ == "__main__":
