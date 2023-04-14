58cf78012ebf2caf1f80208419a8cd59bb3a4416
MegaChie <workspace.mega@gmail.com>
2023-04-14 23:18:02 +0200

    Update 1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,21 +1,23 @@
 #!/usr/bin/python3
-"""  lists all states from the database hbtn_0e_0_usa. """
+""" Script that lists all states from database hbtn_0e_0_usa. """
 import MySQLdb
 import sys
 
 
-def Fetch():
-    """ Filter states. """
-    cofig = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                            passwd=sys.argv[2], db=sys.argv[3])
-    code = config.cursor()
-    code.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%'\
-        ORDER BY id")
-    file = code.fetchall()
-    for lines in file:
-        print(line)
-    code.close()
-    config.close()
+def get_states():
+    """ 1. Filter states. """
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id")
+    rows = cur.fetchall()
+    for lines in rows:
+        print(i)
+
+    cur.close()
+    db.close()
 
 
 if __name__ == "__main__":
