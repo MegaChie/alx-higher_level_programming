aeed746abb5073619a41936e36a3aded60955dd7
MegaChie <workspace.mega@gmail.com>
2023-04-14 23:38:47 +0200

    Update 3-my_safe_filter_states.py

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -8,14 +8,17 @@ import sys
 def get_states():
     """ 3. SQL Injection... """
     if len(sys.argv) == 5:
-        db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                             passwd=sys.argv[2], db=sys.argv[3])
+        db = MySQLdb.connect(host="localhost",
+                             port=3306,
+                             user=sys.argv[1],
+                             passwd=sys.argv[2],
+                             db=sys.argv[3])
         cur = db.cursor()
         cur.execute("SELECT * FROM states WHERE BINARY name='{:s}'\
                     ORDER BY id ASC".format(sys.argv[4]))
-        file = cur.fetchall()
-        for lines in file:
-            print(lines)
+        rows = cur.fetchall()
+        for i in rows:
+            print(i)
         cur.close()
         db.close()
     else:
