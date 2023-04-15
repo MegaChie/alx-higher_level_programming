61160e4610629f9b2a7e24f1fc7c1a044d4ffe6c
MegaChie <workspace.mega@gmail.com>
2023-04-15 02:44:04 +0200

    Update 11-model_state_insert.py

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -8,6 +8,7 @@ from model_state import Base, State
 
 
 def Fetch():
+    """ 11. Add a new state. """
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
