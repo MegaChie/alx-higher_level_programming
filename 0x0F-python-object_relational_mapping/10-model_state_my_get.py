3a46397d33f591a2dfb73476e5abc1afbe8f2487
MegaChie <workspace.mega@gmail.com>
2023-04-15 02:41:18 +0200

    Update 10-model_state_my_get.py

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -8,6 +8,7 @@ from model_state import Base, State
 
 
 def Fetch():
+    """ 10. Get a state. """
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
