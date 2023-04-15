c8b9d1ad5ffd3a4a8bad79ba49c21c5822f10835
MegaChie <workspace.mega@gmail.com>
2023-04-15 03:10:06 +0200

    Update 2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -5,22 +5,19 @@ import MySQLdb
 import sys
 
 
-def get_states():
+def Fetch():
     """ 2. Filter states by user input. """
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
-
     cur = db.cursor()
-
     cur.execute("SELECT * FROM states WHERE BINARY name='{:s}'\
                 ORDER BY id ASC".format(sys.argv[4]))
     file = cur.fetchall()
     for lines in file:
         print(lines)
-
     cur.close()
     db.close()
 
 
 if __name__ == "__main__":
-    get_states()
+    Fetch()
