12a569ec067d3c194b8423be3b3ff7999720fed1
MegaChie <workspace.mega@gmail.com>
2023-10-27 21:02:52 +0200

    Update 6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,15 +3,4 @@
 def find_peak(list_of_integers):
 	temp = list(list_of_integers)
 	number = 0
-	while number != len(temp):
-		if number == 0:
-			if temp[0] > temp[1]:
-				print(temp[0])
-		elif number == len(temp) - 1:
-			if temp[len(temp) - 1] > temp[number - 1]:
-				print(temp[len(temp) - 1])
-		else:
-			if ((temp[number] > temp[number + 1]) and
-				(temp[number] > temp[number - 1])):
-				print(temp[number])
-		number = number + 1
+	print(typeof(temp))
