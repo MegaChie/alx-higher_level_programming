ee747556e772f0a8e3f08628161be72a68e60351
MegaChie <workspace.mega@gmail.com>
2023-10-27 21:44:44 +0200

    Update 100-status_code.sh

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # comment text
-curl -sX DELETE "$1"
+curl -sI "$1"
--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """ Find a peak """
 def find_peak(list_of_integers):
-	number = 0
-	print(len(list_of_integers))
+	n = 0
+	if len(list_of_integers) == 1:
+		print(list_of_integers[0])
+	if 
