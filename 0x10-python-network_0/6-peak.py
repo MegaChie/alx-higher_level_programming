7ed3b3391342484f02a37d4616de63512e120f2a
MegaChie <workspace.mega@gmail.com>
2023-10-28 16:20:01 +0200

    Update 6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,8 +4,8 @@ def find_peak(list_of_integers):
 	i = len(list_of_integers)
 	tmp = (list_of_integers)
 	for n in tmp:
-		if n > tmp[index(n) + 1]:
-			if n > tmp[index(n) - 1]:
-				print(tmp[n])
+		if n > tmp[tmp.index(n) + 1]:
+			if n > tmp[tmp.index(n) - 1]:
+				print(n)
 		if n == tmp[-1]:
 			pass
