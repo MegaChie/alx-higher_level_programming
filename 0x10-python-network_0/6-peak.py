1fab996829de14c2fe2a4ac0f65e376cb1c5131c
MegaChie <workspace.mega@gmail.com>
2023-10-28 16:02:08 +0200

    Update 6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,9 +2,11 @@
 """ Find a peak """
 def find_peak(list_of_integers):
 	i = len(list_of_integers)
-	if i <= 5:
+	if i <= 6:
 		tmp = list(list_of_integers)
-	elif i > 5:
-		tmp = list_of_integers[0:5]
-	print(list_of_integers)
-	print(tmp)
+	elif i > 6:
+		tmp = list_of_integers[0:6]
+	for n in range(len(tmp)):
+		if tmp[n] > tmp[n + 1]:
+			if tmp[n] > tmp[n - 1]:
+				print(tmp[n])
