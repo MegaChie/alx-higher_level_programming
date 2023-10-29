448bee68a957a532cbecb3cfd125e408c8b30b85
MegaChie <workspace.mega@gmail.com>
2023-10-29 20:51:31 +0200

    Update 6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -9,12 +9,12 @@ def find_peak(list_of_integers):
     if len(tmp) == 0:
         return 0
     else:
-        if tmp[0] > tmp[1]:
+        if tmp[0] >= tmp[1]:
             if tmp[0] > tmp[-1]:
                 print(tmp[0])
         else:
             for place in range(1, len(tmp)):
-                if tmp[place] > tmp[place + 1]:
+                if tmp[place] >= tmp[place + 1]:
                     if tmp[place] > tmp[place - 1]:
                         print(tmp[place])
                 else:
