74a605bc8676599df88a32f067b996735d1d3ef6
MegaChie <workspace.mega@gmail.com>
2023-11-01 15:25:04 +0200

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,4 +4,4 @@
 import urllib.request
 req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
 with urllib.request.urlopen(req) as response:
-the_page = response.read()
+	the_page = response.read()
