3924f974c8464ea576063573dc41110a168f3feb
MegaChie <workspace.mega@gmail.com>
2023-11-01 19:14:02 +0200

    Update 1-hbtn_header.py

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -6,11 +6,11 @@ import urllib.request
 import sys
 
 
-def execute():
+def program():
     """Comment text"""
     with urllib.request.urlopen(sys.argv[1]) as response:
         print(response.getheader('X-Request-Id'))
 
 
 if __name__ == "__main__":
-    execute()
+    program()
