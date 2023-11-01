47c64e337783c80ae152914ec2a763bd8584477c
MegaChie <workspace.mega@gmail.com>
2023-11-01 20:20:41 +0200

    Update 2-post_email.py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -3,13 +3,22 @@
 
 
 import urllib.request
+import urllib.parse
 import sys
 
 
 def program():
     """Comment text"""
-    with urllib.request.urlopen(sys.argv[1]) as response:
-        print(response.getheader('X-Request-Id'))
+    url = sys.argv[1]
+    value{
+    "email":sys.argv[2]
+    }
+    string = urllib.parse.urlencode(value)
+    data = string.encode("ascii")
+    with urllib.request.urlopen(url, data) as response:
+        text = response.read()
+        print(text)
+        
 
 
 if __name__ == "__main__":
