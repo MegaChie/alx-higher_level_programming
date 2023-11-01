9594c81cfc103c56b839d638fdc516ea7a36e8a0
MegaChie <workspace.mega@gmail.com>
2023-11-01 15:32:03 +0200

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,5 +7,5 @@ import urllib.request
 with urllib.request.urlopen('http://python.org/') as response:
 	with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
 		shutil.copyfileobj(response, tmp_file)
-with open(tmp_file.name) as html:
-	print(html)
+with open("tmp_file.name", "r") as html:
+	print str(html)
