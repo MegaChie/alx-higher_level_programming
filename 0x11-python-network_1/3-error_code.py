16eba82d08f196e28496519900987c2c86be7d8d
MegaChie <workspace.mega@gmail.com>
2023-11-04 14:27:46 +0200

    Update 0-hbtn_status.py

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -9,13 +9,7 @@ import sys
 
 def program():
     """Comment text"""
-    sent = {'email': sys.argv[2]}
-    data = urllib.parse.urlencode(sent)
-    data = data.encode("ascii")
-    req = urllib.request.Request(sys.argv[1], data)
-    with urllib.request.urlopen(req) as answer:
-        html = answer.read()
-        print(html.decode("utf-8"))
+    
 
 
 if __name__ == "__main__":
