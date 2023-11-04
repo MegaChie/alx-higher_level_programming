b6c3a8ac3da1da982da87b390d376b41a0106972
MegaChie <workspace.mega@gmail.com>
2023-11-04 14:45:11 +0200

    Update 3-error_code.py

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -9,7 +9,12 @@ import sys
 
 def program():
     """Comment text"""
-    
+    try:
+        with urllib.request.urlopen(sys.argv[1]) as answer:
+            html = response.read()
+            print(html.decode("utf-8"))
+    except urllib.error.HTTPError as fetal:
+        print("Error code: {}".format(fetal.code))
 
 
 if __name__ == "__main__":
