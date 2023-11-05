29f8f0f1af844d44d24ffb20c8f1d1c92228c53f
MegaChie <workspace.mega@gmail.comclear>
2023-11-05 01:32:01 -0800

    Add 4-hbtn_status.py

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Error code #0"""
+import urllib.request
+import urllib.parse
+import sys
+
+
+def program():
+    """Comment text"""
+    try:
+        with urllib.request.urlopen(sys.argv[1]) as answer:
+            html = answer.read()
+            print(html.decode("utf-8"))
+    except urllib.error.HTTPError as fetal:
+        print("Error code: {}".format(fetal.code))
+
+
+if __name__ == "__main__":
+    program()
