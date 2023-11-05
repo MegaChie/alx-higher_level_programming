ceb9759afb28dfe7e8546c5c9ee83f2c685d6045
MegaChie <workspace.mega@gmail.comclear>
2023-11-05 05:07:04 -0800

    Add 8-json_api.py

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Error code"""
+import requests
+import sys
+
+
+def program():
+    """Comment text"""
+    polo = requests.get(sys.argv[1])
+    if polo.status_code >= 400:
+        print("Error code: {}".format(polo.status_code))
+    else:
+        pass
+
+
+if __name__ == "__main__":
+    program()
