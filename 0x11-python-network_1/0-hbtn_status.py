371e59744ff30c2c9fe2be7437b68ae68063388f
MegaChie <workspace.mega@gmail.com>
2023-11-05 12:00:57 +0200

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,6 +4,7 @@ import urllib.request
 
 
 def program():
+    """Program code"""
     with urllib.request.urlopen("https://intranet.hbtn.io/status") as response:
         html = response.read()
         print("Body response:")
