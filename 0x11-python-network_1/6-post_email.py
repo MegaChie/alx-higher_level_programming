c53913a36d407a31ec34f3d0ba4cf331fc3b525f
MegaChie <workspace.mega@gmail.comclear>
2023-11-05 04:25:36 -0800

    Add 6-post_email.py

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""What's my status?"""
+import requests
+import sys
+
+
+def program():
+    """Comment text"""
+    polo = requests.get(sys.argv[1])
+    print(polo.headers.get('X-Request-Id'))
+
+
+if __name__ == "__main__":
+    program()
