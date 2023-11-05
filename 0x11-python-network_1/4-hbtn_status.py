997bc07be7ff6108ba738905c6d4a7ae09ea49f4
MegaChie <workspace.mega@gmail.com>
2023-11-05 11:44:25 +0200

    Update 4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,10 +5,10 @@ import requests
 
 def program():
     """Comment text"""
-    with requests.get("https://alx-intranet.hbtn.io/status") as answer:
-        print("Body response:")
-        print("\t- type: {}".format(type(answer)))
-        print("\t- content: {}".format(answer))
+    answer = requests.get("https://alx-intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(answer)))
+    print("\t- content: {}".format(answer))
 
 
 if __name__ == "__main__":
