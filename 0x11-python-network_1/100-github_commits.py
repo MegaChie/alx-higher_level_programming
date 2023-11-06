4f38ac021a6a6b91680aed247b5145e24be0dcec
MegaChie <workspace.mega@gmail.comclear>
2023-11-06 03:19:43 -0800

    Add 100-github_commits.py

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""My GitHub!"""
+import requests
+from requests.auth import HTTPBasicAuth
+import sys
+
+
+def program():
+    """Comment text"""
+    name, pkey = sys.argv[1], sys.argv[2]
+    marko = requests.get("https://api.github.com/user",
+                         auth=(HTTPBasicAuth(name, pkey)))
+    try:
+        polo = marko.json()
+        print(polo['id'])
+    except:
+        print("None")
+
+
+if __name__ == "__main__":
+    program()
