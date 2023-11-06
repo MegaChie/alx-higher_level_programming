7336e4f0e495003bdcf691bc9667e06d55b6332b
MegaChie <workspace.mega@gmail.com>
2023-11-06 13:15:27 +0200

    Update 10-my_github.py

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -9,7 +9,7 @@ def program():
     """Comment text"""
     name, pkey = sys.argv[1], sys.argv[2]
     marko = requests.get("https://api.github.com/user",
-        auth=(HTTPBasicAuth(name, pkey)))
+            auth=(HTTPBasicAuth(name, pkey)))
     try:
         polo = marko.json()
         print(polo['id'])
