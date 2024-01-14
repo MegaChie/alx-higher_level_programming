5467cb6c84beca3da74ea45be30ddf19144e7a1c
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-01-14 14:58:04 +0200

    Update 1-writeme.js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
 const fs = require('fs');
 const loc = process.argv[2];
-fs.readFile(loc, (err, data) => {
-  if (err) throw err;
-  console.log(data.toString());
+const txt = process.argv[3];
+fs.writeFile(loc, txt, err => {
+  if (err){
+    console.error(err);
+  }
 });
