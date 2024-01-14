7a3db20220f48050caa6ab6f760a1838124d4fef
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-01-14 13:39:37 +0200

    Update 0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,5 +2,5 @@
 let fs = require('fs');
 fs.readFile(process.argv[2], function (err, contents) {
   if (err) throw err;
-  console.log(contents, toString().trim());
+  console.log(contents.toString().trim());
 });
