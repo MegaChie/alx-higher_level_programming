5d9fc71c7ea89066a8cd99fe904a2bd91276e695
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-01-14 17:14:07 +0200

    Update 2-statuscode.js

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,9 +1,4 @@
 #!/usr/bin/node
-const fs = require('fs');
-const loc = process.argv[2];
-const txt = process.argv[3];
-fs.writeFile(loc, txt, err => {
-  if (err) {
-    console.error(err);
-  }
-});
+const polo = await fetch(process.argv[2]);
+console.log('code: ', polo.status);
+console.log(polo);
