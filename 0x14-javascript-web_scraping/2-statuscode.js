77ae191f59d5336352b122dfd5d83d9b3856c8e3
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-01-14 17:56:37 +0200

    Update 2-statuscode.js

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const request = require('request');
 const requestSettings = {
-    url: process.argv[2]
+  url: process.argv[2]
 };
-request(requestSettings, function(error, response, body) {
-    console.log(response.statusCode);
-});   
+request (requestSettings, function(error, response, body) {
+  console.log(response.statusCode);
+});
