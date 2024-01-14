f7737cdc6c5ab04e54ae1c962a625947892cc647
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-01-14 15:37:00 +0200

    Update 1-writeme.js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,8 +2,8 @@
 const fs = require('node:fs/promises');
 const loc = process.argv[2];
 const txt = process.argv[3];
-fs.writeFile(loc, txt, { flag: 'a+'}, err => {
-  if (err){
+fs.writeFile(loc, txt, { flag: 'a+' }, err => {
+  if (err) {
     console.error(err);
   }
 });
