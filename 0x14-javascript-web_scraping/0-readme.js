40c2626ea17c839707b32820795922586517135e
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-01-14 14:38:51 +0200

    Update 0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
 const fs = require('fs');
-fs.readFile(process.argv[2], function(err, data) {
+const loc = process.argv[2]
+fs.readFile(loc, function(err, data) {
   if (err) throw err;
   console.log(data.toString());
 });
