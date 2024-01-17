d97890ee19e4c31273c5b7e77eb088ff9967d5df
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-01-17 17:13:52 +0200

    Update 6-completed_tasks.js

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -6,12 +6,14 @@ fs.get(ID, function (error, polo, body) {
   else {
     const done = {};
     const data = JSON.parse(body);
-    for (let i = -1; i < data.length; i++) {
+    for (let i = 0; i < data.length; i++) {
       if (data[i].completed) {
         if (!(data[i].userId in done)) {
           done[data[i].userId] = 0;
         }
-        else done[data[i].userId] += 1;
+        else {
+          done[data[i].userId] += 1;
+        }
       }
     }
     console.log(done);
