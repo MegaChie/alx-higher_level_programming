cf3351d8a6399b82adc6e86976b6dcf559f725f3
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-01-17 23:32:47 +0200

    Update 100-starwars_characters.js

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,19 +1,15 @@
 #!/usr/bin/node
 const ID = process.argv[2];
+const link = 'https://swapi-api.alx-tools.com/api/films/' + ID
 const fs = require('request');
-fs.get(ID, function (error, polo, body) {
-  if (error) console.log(error);
+fs.get(link, function (error, polo, body) {
+  if (error) throw error;
   else {
-    let count = 0;
-    const data = JSON.parse(body).results;
-    for (let i = 0; i < data.length; i++) {
-      for (let j = 0; j < data[i].characters.length; j++) {
-        if (data[i].characters[j].includes('/18/')) {
-          count++;
-          break;
-        }
-      }
+    const data = JSON.parse(body).characters;
+    for (let i = 0; i < data.length; i++){
+      fs.get(data[i], function (error, polo, body) {
+        console.log(JSON.parse(body).name);
+      });
     }
-    console.log(count);
   }
 });
