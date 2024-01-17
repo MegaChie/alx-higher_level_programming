dcce9bdf7f86dc11696adecc2db71f77a6172751
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-01-17 13:00:11 +0200

    Update 3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,5 +1,9 @@
 #!/usr/bin/node
-const base = "https://swapi-api.alx-tools.com/api/films/";
-const ID = process.argv[2];
+const base = 'https://swapi-api.alx-tools.com/api/films/';
+const ID = process.argv[2] + '/';
 const link = base.concat(ID);
-console.log(link);
+const fs = require('request');
+fs.get(link, function (error, polo, body) {
+if (error) console.log(error);
+console.log(JSON.parse(polo).title);
+});
