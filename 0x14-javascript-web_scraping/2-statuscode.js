549e45647be9502a657d49e9069459f6bd0ec4bb
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-01-17 23:01:47 +0200

    Update 2-statuscode.js

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const link = process.argv[2];
 const fs = require('request');
-fs.get(link, function (error, polo, body) {
+fs(link, function (error, polo, body) {
   if (error) throw error;
   console.log(polo.status && polo.statusCode);
 });
