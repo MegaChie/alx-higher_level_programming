97450ac87c4a2ebea681d6d32d26473bf3da4a36
MegaChie <workspace.mega@gmail.com>
2024-01-17 13:42:39 -0800

    Add 101-starwars_characters.js

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const ID = process.argv[2];
+const link = 'https://swapi-api.alx-tools.com/api/films/' + ID;
+const fs = require('request');
+fs.get(link, function (error, polo, body) {
+  if (error) throw error;
+  else {
+    const data = JSON.parse(body).characters;
+    for (let i = 0; i < data.length; i++) {
+      fs.get(data[i], function (error, polo, ans) {
+        if (!error) console.log(JSON.parse(ans).name);
+      });
+    }
+  }
+});
