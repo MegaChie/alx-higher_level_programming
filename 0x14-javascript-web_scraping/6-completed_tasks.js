a83d54dc5d506962cf226669354e70fff894c06a
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-01-17 17:24:55 +0200

    Update 6-completed_tasks.js

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -4,8 +4,8 @@ const fs = require('request');
 fs.get(ID, function (error, polo, body) {
   if (error) console.log(error);
   else {
-    const done = {};
-    const data = JSON.parse(body);
+    let done = {};
+    let data = JSON.parse(body);
     for (let i = 0; i < data.length; i++) {
       if (data[i].completed) {
         if (!(data[i].userId in done)) done[data[i].userId] = 0;
