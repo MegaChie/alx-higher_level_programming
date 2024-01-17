8a9c236092e58cd336160b3a9798b6191b924585
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-01-18 00:12:47 +0200

    Update 101-starwars_characters.js

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -6,10 +6,10 @@ fs.get(link, function (error, polo, body) {
   if (error) throw error;
   else {
     const data = JSON.parse(body);
+    console.log(data);
     for (let i = 0; i < data.length; i++) {
       fs.get(data[i], function (error, polo, ans) {
         if (!error) console.log(JSON.parse(ans).name);
-        continue;
       });
     }
   }
