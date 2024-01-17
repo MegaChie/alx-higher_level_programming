cd79312291c26aef6c6cb84f388839b83e1e2ad8
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-01-17 15:09:00 +0200

    Update 4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,7 +4,7 @@ const fs = require('request');
 fs.get(ID, function (error, polo, body) {
   if (error) console.log(error);
   let count = 0;
-  let data = JSON.parse(body).results;
+  const data = JSON.parse(body).results;
   for (let i = 0; i < data.length; i++) {
     for (let j = 0; j < data[i].characters.length; j++) {
       if (data[i].characters[j].includes('/18/')) {
