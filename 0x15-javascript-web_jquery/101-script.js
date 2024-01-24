5b852d9c0df8795c32e8ed9bb983cdbdb228c1cd
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-01-24 13:47:08 +0200

    Update 101-script.js

--- 0x15-javascript-web_jquery/101-script.js
+++ 0x15-javascript-web_jquery/101-script.js
@@ -1,3 +1,11 @@
-$("div#add_item").on("click", function() {
-  $("UL.my_list").append("<li>Item</li>")
-});
+$( document ).ready(function() {
+  $("div#add_item").on("click", function() {
+    $("UL.my_list").append("<li>Item</li>")
+  });
+  $("DIV#remove_item").on("click", function() {
+    $("UL.my_list").last().remove("<li>Item</li>")
+  });
+  $("DIV#clear_list").on("click", function() {
+    $("UL.my_list").empty()
+  });
+}
