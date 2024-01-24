526f85cc232120a7341dc1df542b2ad2a69f0cd2
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-01-24 22:54:49 +0200

    Update 102-script.js

--- 0x15-javascript-web_jquery/102-script.js
+++ 0x15-javascript-web_jquery/102-script.js
@@ -2,7 +2,7 @@ $(document).ready( function() {
   const link = "https://www.fourtonfish.com/hellosalut/?";
   $("INPUT#btn_translate").on("click", function() {
     const lang = {lang: $("INPUT#language_code").val()};
-    $.get(link, $.param(lang), function(json) {
+    $.get(link + $.param(lang), function(json) {
       $("DIV#hello").text(json.hello);
     });
   });
