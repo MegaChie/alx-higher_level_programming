4322cc26824a0e27ed521d9ed4a5297d5a095856
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-01-24 15:08:21 +0200

    Update 102-script.js

--- 0x15-javascript-web_jquery/102-script.js
+++ 0x15-javascript-web_jquery/102-script.js
@@ -1,5 +1,5 @@
 const base = "https://www.fourtonfish.com/hellosalut/hello/";
-const lang = $("INPUT#language_code").text();
+const lang = $("INPUT#language_code").val();
 const link = base + "?lang=" + lang;
 $("INPUT#btn_translate").on("click", function() {
   $.get(link, function(json) {
