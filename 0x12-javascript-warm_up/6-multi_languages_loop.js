cb3ed33ce190f9853877adcb0b0e1c871cfc0fab
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-02-12 13:21:53 +0200

    Update 6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const myVar = ['C is fun\n', 'Python is cool\n', 'JavaScript is amazing'];
+const myVar = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let iter = 0; iter < myVar.length; iter++) {
   console.log(myVar[iter]);
 }
