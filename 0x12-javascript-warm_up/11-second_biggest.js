a18303d269b756e8eeec67dd7d81018819d48ea7
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-02-12 17:04:37 +0200

    Update 11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,7 +4,7 @@ if (process.argv.length === 2 || isNaN(process.argv)) {
 } else {
   const nums = [];
   for (let index = 2; index < process.argv.length; index++) {
-    nums[index - 2] = process.argv[index];    
+    nums.push(process.argv[index]);    
   }
   const big = nums.sort();
   console.log(big[1]);
