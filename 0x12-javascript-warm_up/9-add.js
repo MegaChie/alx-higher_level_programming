49f8b754d7985ef8f3f7ba30b98130bf91f5632a
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-02-12 15:19:33 +0200

    Update 9-add.js

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,8 +1,5 @@
 #!/usr/bin/node
-if (isNaN(process.argv[2]) === false) {
-  for (let index = 0; index < parseInt(process.argv[2]); index++) {
-    console.log('X'.repeat(process.argv[2]));    
-  }
-} else {
-  console.log('Missing size');
+function add(a, b) {
+  console.log(a + b);
 }
+add(parseInt(process.argv[2]), parseInt(process.argv[2]));
