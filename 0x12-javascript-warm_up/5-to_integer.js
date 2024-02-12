2a27870c6590fc6fe64ac2d36747a4de0bdcf29b
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-02-12 13:45:20 +0200

    Update 5-to_integer.js

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-if (typeof(process.argv[2]) === 'number') {
-  console.log('My number: ' + process.argv[2]);
-} else {
+if (isNaN(process.argv[2]) === true) {
   console.log('Not a number');
+} else {
+  console.log('My number: ' + process.argv[2]);
 }
