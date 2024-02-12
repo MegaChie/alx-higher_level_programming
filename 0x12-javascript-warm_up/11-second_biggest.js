d509ca2b5fb3d8f78854b54843397c61d25fdc30
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-02-12 16:56:25 +0200

    Update 11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,5 +1,10 @@
 #!/usr/bin/node
-function add (a, b) {
-  console.log(a + b);
+if (process.argv.length === 2 || isNaN(process.argv)) {
+  console.log(0);
+} else {
+  const nums = [];
+  for (let index = 2; index < process.argv.length; index++) {
+    nums[index - 2] = process.argv[index];    
+  }
+  console.log(nums.sort()[1])
 }
-add(parseInt(process.argv[2]), parseInt(process.argv[3]));
