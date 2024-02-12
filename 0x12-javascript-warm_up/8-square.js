0d923b3ef9eb965b2cd95db38dd186d6f44bbc76
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-02-12 14:58:41 +0200

    Update 8-square.js

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 if (isNaN(process.argv[2]) === false) {
   for (let index = 0; index < parseInt(process.argv[2]); index++) {
-    console.log('C is fun');    
+    console.log('C is fun' * parseInt(process.argv[2]));    
   }
 } else {
-  console.log('Missing number of occurrences');
+  console.log('Missing size');
 }
