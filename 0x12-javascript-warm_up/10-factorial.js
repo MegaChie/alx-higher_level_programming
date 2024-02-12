954265267090efd0f45a6a67fff560d8466e8f7d
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-02-12 16:41:32 +0200

    Update 10-factorial.js

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
 function fact (a) {
-  if (a > 0) {
     return (a * fact(a - 1));
-  }  
-}
+  }
 if (isNaN(process.argv[2]) === false) {
   console.log(fact(parseInt(process.argv[2])));
 } else {
