529f728e14a6635ff136a1c96de0702a9788efb1
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-02-12 12:24:08 +0200

    Update 2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-if (process.argv.length < 3) {
+if (process.argv.length < 2) {
     console.log('No argument');
-} else if (process.argv.length == 3) {
+} else if (process.argv.length == 2) {
     console.log(process.argv[2]);
 } else {
     console.log('Arguments found');
