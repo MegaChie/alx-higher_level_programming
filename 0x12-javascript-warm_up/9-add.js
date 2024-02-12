396a31f76d55ec26cbf67ac1467ff6bb9fdf2e9e
MegaChie <workspace.mega@gmail.com>
2024-02-12 05:10:47 -0800

    Add 9-add.js

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+if (isNaN(process.argv[2]) === false) {
+  for (let index = 0; index < parseInt(process.argv[2]); index++) {
+    console.log('X'.repeat(process.argv[2]));    
+  }
+} else {
+  console.log('Missing size');
+}
