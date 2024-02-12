ff5cad1ee6b4810cce4692c6137333cc3610cf68
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-02-12 14:16:55 +0200

    Update 7-multi_c.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,5 +1,8 @@
 #!/usr/bin/node
-const myVar = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (let iter = 0; iter < myVar.length; iter++) {
-  console.log(myVar[iter]);
+if (isNaN(process.argv[2]) === false) {
+  for (let index = 0; index < parseInt(process.argv[2]); index++) {
+    console.log('C is fun');    
+  }
+} else {
+  console.log('Missing number of occurrences');
 }
