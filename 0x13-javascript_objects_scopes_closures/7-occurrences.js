1d612b194ce9f261f13685ab3fa657208e59fccc
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-02-13 14:22:24 +0200

    Update 7-occurrences.js

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,2 +1,10 @@
 #!/usr/bin/node
-module.exports = class Rectangle {};
+exports.nbOccurences = function (list, searchElement) {
+  let ocurCount = 0;
+  for (let index = 0; index < list.length; index++) {
+    if (list[index] === searchElement) {
+      ocurCount++;
+    }
+  }
+  return ocurCount;
+};
