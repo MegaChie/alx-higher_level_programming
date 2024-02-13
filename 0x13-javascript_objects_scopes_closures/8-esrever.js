4fa1b9349eea5ec507e2b1bc0886528b4f0406cc
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-02-13 14:33:45 +0200

    Update 8-esrever.js

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
   const reveList = [];
-  for (let index = list.length - 1; index > 0; index--) {
+  for (let index = list.length - 1; index >= 0; index--) {
     reveList.push(list[index]);
   }
   return reveList;
