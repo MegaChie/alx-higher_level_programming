be62ab872cefa83e6f24eed29cdc1c7c090830eb
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-02-13 19:22:45 +0200

    Update 101-sorted.js

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -3,6 +3,9 @@ const oldDict = require('./101-data.js').dict;
 const newDict = {};
 const key = Object.keys(oldDict)
 for (let index = 0; index < key.length; index++) {
+  if (newDict[oldDict[key[index]]].push(key[index]) === undefined) {
+    newDict[oldDict[key[index]]] = [];
+  }
   newDict[oldDict[key[index]]].push(key[index]);
 }
 console.log(newDict);
