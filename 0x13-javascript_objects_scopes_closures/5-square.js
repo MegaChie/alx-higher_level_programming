62a734770eb58729599e5ceac880e84e12b41d28
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-02-13 14:01:42 +0200

    Update 5-square.js

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
-module.exports = class Rectangle {
-  constructor (w, h) {
-    this.width = w;
-    this.height = h;
+module.exports = class Square extends require('./4-rectangle.js') {
+  constructor (size) {
+    super(size, size);
   }
 };
