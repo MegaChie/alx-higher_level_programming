86a8e465c10aabc9b6298eb72439b8d4586a1825
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-02-13 21:34:33 +0200

    Update 102-concat.js

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -3,4 +3,4 @@ const fs = require('fs');
 const cont1 = fs.readFileSync(process.argv[2], 'utf-8');
 const cont2 = fs.readFileSync(process.argv[3], 'utf-8');
 const toWrit = cont1 + cont2;
-fs.writeFile(process.argv[4], toWrit, { flag: 'w+' });
+fs.writeFile(process.argv[4], toWrit);
