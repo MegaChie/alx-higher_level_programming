df795340969d3cf9dad36258ee772a9788bf30ba
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-02-13 21:02:04 +0200

    Update 102-concat.js

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,2 +1,5 @@
 #!/usr/bin/node
-module.exports = class Rectangle {};
+const fs = require('fs');
+const cont1 = fs.readFile(process.argv[2], 'utf-8');
+const cont2 = fs.readFile(process.argv[3], 'utf-8');
+fs.writeFile(process.argv[4], a + b);
