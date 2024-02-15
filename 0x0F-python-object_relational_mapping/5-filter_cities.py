603d9435bd6b0c469ee1c9f5a830ec43305a39a8
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-02-16 00:07:39 +0200

    Update 5-filter_cities.py

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -14,5 +14,5 @@ if __name__ == "__main__":
               and states.name = '{}' order by cities.id;""".format(sys.argv[4])
     curs.execute(ques)
     result = curs.fetchall()
-    print(*result)
+    print(*result.replace("\'", ""))
     conn.close()
