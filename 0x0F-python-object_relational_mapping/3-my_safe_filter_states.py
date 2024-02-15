ffa6f599607c088ba996572a849089167de1d8f1
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-02-15 15:33:31 +0200

    Update 3-my_safe_filter_states.py

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -5,13 +5,13 @@ import sys
 
 
 if __name__ == "__main__":
-    """Task #3"""
+    """Task #4"""
     conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                            passwd=sys.argv[2], db=sys.argv[3])
     curs = conn.cursor()
-    curs.execute("select * from states where name like {} order by id"
-                 .format(sys.argv[4]))
+    curs.execute("select * from states where name={} order by id"
+                 .format(sys.argv[4].split(";")[0]))
     result = curs.fetchall()
     for line in result:
         print(line)
-    curs.close()
+    conn.close()
