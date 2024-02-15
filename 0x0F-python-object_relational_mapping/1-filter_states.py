f2ebef11b75f885250aa8ed61992a52a4e652e10
MegaChie <workspace.mega@gmail.com>
2024-02-15 03:25:02 -0800

    Add 1-filter_states.py

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Using MySQLdb to connect and query a database"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    """Using MySQLdb to connect to a database"""
+    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                        passwd=sys.argv[2], db=sys.argv[3])
+    curs = conn.cursor()
+    curs.execute("select * from states order by id")
+    result = curs.fetchall()
+    for line in result:
+        print(line)
+    curs.close()
