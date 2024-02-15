81bf49a391a4261fb56f8bf25e16b8241fb8d8aa
MegaChie <workspace.mega@gmail.com>
2024-02-15 05:38:47 -0800

    Add 4-cities_by_state.py

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Using MySQLdb to connect and query a database"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    """Task #4"""
+    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3])
+    curs = conn.cursor()
+    curs.execute("select * from states where name={} order by id"
+                 .format(sys.argv[4].split(";")[0]))
+    result = curs.fetchall()
+    for line in result:
+        print(line)
+    conn.close()
