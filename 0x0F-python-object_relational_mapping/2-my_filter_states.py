a4852e3cf86edd8bd7b61fef00080bd3d6973c15
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-02-15 21:56:08 +0200

    Update 2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -9,9 +9,10 @@ if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                            passwd=sys.argv[2], db=sys.argv[3])
     curs = conn.cursor()
-    curs.execute("select * from states where name = {} order by id"
-                 .format(sys.argv[4]))
-    result = curs.fetchall()
-    for line in result:
-        print(line)
-    conn.close()
+    print(sys.argv[4])
+    # curs.execute("select * from states where name = '{} order by id"
+    #              .format(sys.argv[4]))
+    # result = curs.fetchall()
+    # for line in result:
+    #     print(line)
+    # conn.close()
