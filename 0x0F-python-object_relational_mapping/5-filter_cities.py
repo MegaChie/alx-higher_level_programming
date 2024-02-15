f500ea672d02587bc807ffaa459d42a247982e7c
MegaChie <workspace.mega@gmail.com>
2024-02-15 12:32:28 -0800

    Add 5-filter_cities.py

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Using MySQLdb to connect and query a database"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    """Task #5"""
+    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3])
+    curs = conn.cursor()
+    ques = """select cities.id, cities.name, states.name from cities
+              join states on cities.state_id = states.id
+              order by cities.id;"""
+    curs.execute(ques)
+    result = curs.fetchall()
+    for line in result:
+        print(line)
+    conn.close()
