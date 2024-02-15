c1727d328cae558ca3b944d8e632181b1fc8cc6b
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-02-15 22:11:28 +0200

    Update 4-cities_by_state.py

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -9,7 +9,8 @@ if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                            passwd=sys.argv[2], db=sys.argv[3])
     curs = conn.cursor()
-    curs.execute("select * from cities order by id")
+    ques = """select * from cities where cities.id = states.id"""
+    curs.execute(ques)
     result = curs.fetchall()
     for line in result:
         print(line)
