b8c94575e2d3147c872308241248cd5e44a5e30c
MegaChie <workspace.mega@gmail.com>
2024-02-16 14:11:47 -0800

    Add 100-relationship_states_cities.py

---
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""SQLAlchemy quering"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from model_state import Base, State
+from model_city import City
+
+
+if __name__ == "__main__":
+    """task #15"""
+    eng = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                        .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                        pool_pre_ping=True)
+    Base.metadata.create_all(eng)
+    sess = Session(eng)
+    result = sess.query(State, City).join(City).all()
+    for item in result:
+        print("{}: ({}) {}".format(item[0].__dict__["name"],
+              item[1].__dict__["id"], item[1].__dict__["name"]))
+    sess.close()
