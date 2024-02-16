8d061b79c02ff3356cc438271717de98a585912e
MegaChie <workspace.mega@gmail.com>
2024-02-16 11:15:46 -0800

    Add 13-model_state_delete_a.py

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""SQLAlchemy quering"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from sqlalchemy import update
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+    """task #11"""
+    eng = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                        .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                        pool_pre_ping=True)
+    Base.metadata.create_all(eng)
+    sess = Session(eng)
+    replace = update(State)
+    replace = replace.values({"name": "New Mexico"})
+    replace = replace.where(State.id == 2)
+    sess.execute(replace)
+    sess.commit()
+    sess.close()
