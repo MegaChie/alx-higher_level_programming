2021028bdc1760b67cce1bded42509ace5d01809
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-02-16 17:07:30 +0200

    Update 8-model_state_fetch_first.py

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -15,8 +15,7 @@ if __name__ == "__main__":
     sess = Session(eng)
     result = sess.query(State).first()
     if result:
-        for item in result:
-            print("{}: {}".format(item.__dict__['id'], item.__dict__['name']))
+        print("{}: {}".format(item.__dict__['id'], item.__dict__['name']))
     else:
         print("Nothing")
     sess.close()
