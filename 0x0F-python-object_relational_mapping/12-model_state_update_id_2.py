1fe45be0f7753d407a2596525e06d3c7cfa5558b
MegaChie <workspace.mega@gmail.com>
2024-02-16 10:44:45 -0800

    Add 12-model_state_update_id_2.py

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""SQLAlchemy quering"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+    """task #11"""
+    eng = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                        .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                        pool_pre_ping=True)
+    Base.metadata.create_all(eng)
+    sess = Session(eng)
+    stat = State(name="Louisiana")
+    sess.add(stat)
+    sess.commit()
+    result = sess.query(State).all()
+    for item in result:
+        if "Louisiana" in item.__dict__["name"]:
+            print(item.__dict__["id"])
+    sess.close()
