04dc2e5c25654b1629d6b19a9bf61bba67f2aab4
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-02-16 14:10:01 +0200

    Update 2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
                            passwd=sys.argv[2], db=sys.argv[3])
     curs = conn.cursor()
     # print(sys.argv[4])
-    curs.execute("select * from states where name = '{}' order by id"
+    curs.execute("select * from states where BINARY '{}' order by id"
                  .format(sys.argv[4]))
     result = curs.fetchall()
     for line in result:
