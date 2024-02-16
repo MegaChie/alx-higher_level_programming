1b4e822f8f2a75d6929cf8451f1161bb4fe18b55
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-02-16 22:40:36 +0200

    Update 3-my_safe_filter_states.py

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -7,13 +7,13 @@ from model_state import Base, State
 
 
 if __name__ == "__main__":
-    """task #8"""
+    """task #15"""
     eng = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
                         .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                         pool_pre_ping=True)
     Base.metadata.create_all(eng)
     sess = Session(eng)
-    result = sess.query(State, City).join(State).all()
+    result = sess.query(State, City).join(City).all()
     for item in result:
         print("{}: ({}) {}".format(item[0].__dict__["name"],
               item[1].__dict__["id"], item[1].__dict__["name"]))
