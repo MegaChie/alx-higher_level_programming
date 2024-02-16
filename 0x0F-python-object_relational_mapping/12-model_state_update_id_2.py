cf253094d0d7214692e875d33c14c5f04c42a0b1
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-02-16 21:17:26 +0200

    Update 12-model_state_update_id_2.py

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -8,7 +8,7 @@ from model_state import Base, State
 
 
 if __name__ == "__main__":
-    """task #11"""
+    """task #13"""
     eng = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
                         .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                         pool_pre_ping=True)
