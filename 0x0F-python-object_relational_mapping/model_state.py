753ab7a1540700e839676076f266de83d522e91d
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-02-16 14:43:10 +0200

    Update model_state.py

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -7,7 +7,7 @@ Base = declarative_base()
 
 
 class State(Base):
+    """task #7"""
     __tablename__ = "states"
-
     id = Column(Integer, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
