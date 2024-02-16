b11656115db4dfcff75ca245c63c215c386ea254
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-02-16 17:54:23 +0200

    Update 10-model_state_my_get.py

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -7,15 +7,20 @@ from model_state import Base, State
 
 
 if __name__ == "__main__":
-    """task #9"""
+    """task #11"""
     eng = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
                         .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                         pool_pre_ping=True)
     Base.metadata.create_all(eng)
     sess = Session(eng)
     result = sess.query(State).all()
+    stateList = []
     for item in result:
-        if "a" in item.__dict__["name"]:
-            print("{}: {}"
-                  .format(item.__dict__["id"], item.__dict__["name"]))
+        if sys.argv[4] in item.__dict__["name"]:
+            stateList.append(item.__dict__["id"])
+    if len(stateList) == 0:
+        print("Not found")
+    else:
+        for elem in stateList:
+            print(elem)
     sess.close()
