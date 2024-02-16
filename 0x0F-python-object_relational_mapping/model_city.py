a518494498659a2cdfe52990bc1a6e6356c67043
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-02-16 22:13:40 +0200

    Update model_city.py

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """SQLAlchemy classes"""
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-Base = declarative_base()
+from sqlalchemy import Column, ForeignKey, Integer, String
+from model_state import Base
 
 
-class State(Base):
-    """task #7"""
-    __tablename__ = "states"
+class City(Base):
+    """task #15"""
+    __tablename__ = "cities"
     id = Column(Integer, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
