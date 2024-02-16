0972551e2b6c95775c3f9310d27f1fe63251deb0
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-02-16 14:19:07 +0200

    Update 3-my_safe_filter_states.py

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -6,16 +6,15 @@ import sys
 
 if __name__ == "__main__":
     """Task #3"""
-    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                           passwd=sys.argv[2], db=sys.argv[3])
-    curs = conn.cursor()
-    stat = """select * from states
-              where name like BINARY '{}'
-              order by id""".format(sys.argv[4])
-    # print(sys.argv[4])
-    curs.execute("select * from states where name like BINARY '{}' order by id"
-                 .format(sys.argv[4]))
-    result = curs.fetchall()
-    for line in result:
-        print(line)
-    conn.close()
+    if len(sys.argv) == 5:
+        conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                            passwd=sys.argv[2], db=sys.argv[3])
+        curs = conn.cursor()
+        stat = """select * from states
+                where name like BINARY '{}'
+                order by id""".format(sys.argv[4])
+        curs.execute(stat)
+        result = curs.fetchall()
+        for line in result:
+            print(line)
+        conn.close()
