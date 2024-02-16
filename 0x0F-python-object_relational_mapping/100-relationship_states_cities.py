873ad4439ba5b5f9dfbe4d004217c9ba81027afe
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-02-17 01:21:17 +0200

    Update 100-relationship_states_cities.py

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -13,8 +13,8 @@ if __name__ == "__main__":
     eng = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
                         .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                         pool_pre_ping=True)
-    Base.metadata.create_all(enge)
-    Session = sessionmaker(bind=enge)
+    Base.metadata.create_all(eng)
+    Session = sessionmaker(bind=eng)
     session = Session()
     newState = State(name='California')
     newCity = City(name='San Francisco')
