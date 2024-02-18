29e913e0cdb085172f7cf41afdc7b5b7ff1c194c
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-02-18 23:01:59 +0200

    Update 100-relationship_states_cities.py

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,17 +1,25 @@
 #!/usr/bin/python3
 """SQLAlchemy quering"""
-import sys
+from relationship_state import Base, State
+from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
-from model_state import Base, State
-from model_city import City
-
+from relationship_city import City
+from sys import argv
 
 if __name__ == "__main__":
     """task #16"""
-    eng = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                        .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                        pool_pre_ping=True)
-    Base.metadata.create_all(eng)
-    sess = Session(eng)
-    sess.close()
+
+    db = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(argv[1], argv[2],
+                                                          argv[3])
+    engine = create_engine(db)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    new_sname = "California"
+    new_cname = "San Francisco"
+    new_state = State(name=new_sname)
+    new_city = City(name=new_cname)
+    new_state.cities.append(new_city)
+    session.add(new_state)
+    session.commit()
+    session.close()
