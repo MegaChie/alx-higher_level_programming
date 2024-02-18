7a7f470a2ce87b764eb4dac1fb809b201b832de2
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-02-19 00:14:24 +0200

    Update 3-my_safe_filter_states.py

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -7,7 +7,7 @@ from sys import argv
 if __name__ == "__main__":
     """task #4"""
     conn = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
-                         passwd=argv[2], db=argv[3])
+                           passwd=argv[2], db=argv[3])
     curs = conn.cursor()
     curs.execute("SELECT * FROM states \
                  WHERE name LIKE BINARY %(name)s \
