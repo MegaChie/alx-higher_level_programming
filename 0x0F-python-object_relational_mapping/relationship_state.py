02ea96d8102916c8c7aeb118fbe21e151923fd2b
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-02-18 23:23:17 +0200

    Update relationship_state.py

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """SQLAlchemy classes"""
-from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String
+
 Base = declarative_base()
 
 
 class State(Base):
-    """task #16"""
+    """A class that defines State insatnces"""
+
     __tablename__ = "states"
-    id = Column(Integer, nullable=False, primary_key=True)
+    id = Column(Integer, autoincrement=True, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state")
