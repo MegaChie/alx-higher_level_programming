e868fa3048be37c18657f1904163dfabd1538010
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-02-29 15:03:38 +0200

    Update 5-post_params.sh

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sends a GET request to the URL, and displays the body of the response
-curl -sH 'X-School-User-Id: 98' "$1"
+# Sends a POST request to the passed URL and displays the body of the response
+curl -sd "email=test@gmail.com&subject=I will always be here for PLD" "$1"
