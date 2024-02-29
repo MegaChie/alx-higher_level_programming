8b4233ff7e58de7cd6c63faab6e58bedb0deb4b6
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-02-29 12:29:07 +0200

    Update 0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Takes in a URL, sends a request, and displays the size of the response
-curl "$1" | grep -i Content-Length | awk '{print $2}'
+curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
