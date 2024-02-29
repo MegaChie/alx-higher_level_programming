8b8ec4a2093eb7a56551657e6faa6f54ce63e826
MegaChie <workspace.mega@gmail.com>
2024-02-29 13:19:05 -0800

    Add 101-post_json.sh

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sends a JSON POST request to URL passed and displays the response's body
-curl -sH "Content-Type: application/json" --data "@$2" "$1"
+# Sends a request to the URL passed and displays response's status code.
+curl -s -o /dev/null -w "%{http_code}" "$1"
