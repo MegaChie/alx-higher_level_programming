0cf4de6d25c899764119a529f630bfee6f44e9c0
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-02-29 14:15:17 +0200

    Update 3-methods.sh

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sends a DELETE request and displays the body of the response
-curl -sX DELETE "$1"
+# Takes in a URL and displays all HTTP methods the server will accept
+curl -sI OPTIONS "$1" | grep "Allow: " | sed 's/Allow: //'
