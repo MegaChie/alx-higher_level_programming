d3db9fe293f3da785b06e2b3adc2c88d9e452d07
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-02-29 23:51:16 +0200

    Update 102-catch_me.sh

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # Makes a request to 0.0.0.0:5000/catch_me
 # that causes the server to respond with a message containing "You got me!"
-curl -X PUT 0.0.0.0:5000/catch_me -d "You got me!"
+curl -X PUT 0.0.0.0:5000/catch_me
+   -H "Content-Type: text/html; charset=UTF-8" 
+   -d "You got me!"
 curl -sL 0.0.0.0:5000/catch_me
