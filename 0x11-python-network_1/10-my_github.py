51e8b5792fd7f7708d86af313e8539382cf9f6e8
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-03-02 01:29:31 +0200

    Update 10-my_github.py

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -2,7 +2,7 @@
 """9. My GitHub!"""
 import sys
 import requests as req
-from requests.auth import HTTPBasicAuth
+from requests.auth import HTTPBasicAuth as login
 
 if __name__ == "__main__":
     """
@@ -10,7 +10,12 @@ if __name__ == "__main__":
     It does not use HTTPBasicAuth, using it requires a PAT.
     It is imported only for the checker will look for it.
     """
-    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
-    r = req.get("https://api.github.com/user", auth=auth)
-    print(r.json().get("id"))
+    link = """https://api.github.com/{}""".format(sys.argv[1])
+    # name
+    name = sys.argv[1]
+    # password
+    phra = sys.argv[2]
+    with req.get(link, auth=login(name, phra)) as marko:
+        polo = marko.json()
+        print(polo)
         # print(polo["items"][0].get("id"))
