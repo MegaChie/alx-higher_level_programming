89c6b45150d2bdac1d2c3af4dacdf4bf101fe655
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-03-02 00:32:55 +0200

    Update 10-my_github.py

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 """9. My GitHub!"""
 import sys
-import requests as req
+import requests
 from requests.auth import HTTPBasicAuth
 
+
 if __name__ == "__main__":
-    """
-    Takes GitHub credentials and uses the GitHub API to display ID.
-    """
     auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
-    r = req.get("https://api.github.com/user", auth=auth)
+    r = requests.get("https://api.github.com/user", auth=auth)
     print(r.json().get("id"))
-        # print(polo.get("id"))
