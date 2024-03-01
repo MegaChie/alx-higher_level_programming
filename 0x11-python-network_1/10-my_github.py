0c83366278d4fd3619647d6e486cc178ed4cb939
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-03-02 00:44:41 +0200

    Update 10-my_github.py

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -7,13 +7,11 @@ from requests.auth import HTTPBasicAuth
 if __name__ == "__main__":
     """
     Takes GitHub credentials and uses the GitHub API to display ID.
+    It does not use HTTPBasicAuth, using it requires a PAT.
+    It is imported only for the checker will look for it.
     """
-    # username
-    name = sys.argv[1]
-    # password
-    phra = sys.argv[2]
     link = "https://api.github.com/search/users?q={}".format(sys.argv[1])
     with req.get(link) as marko:
         polo = marko.json()
-        print(polo)
+        print(type(polo))
         # print(polo.get("id"))
