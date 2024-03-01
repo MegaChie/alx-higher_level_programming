0190ea2e952d753fcde7cdea764649c6bb312afa
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-03-01 18:46:01 +0200

    Update 8-json_api.py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """8. Search API"""
-import json
 import sys
 import requests as req
 
@@ -8,8 +7,8 @@ import requests as req
 if __name__ == "__main__":
     """
     Sends a POST request url with a letter as a parameter and returns either:
-    - Not a valid JSON.
-    - Name and ID of the person whos name starts with the letter
+        - Not a valid JSON.
+        - Name and ID of the person whos name starts with the letter
     """
     link = "http://0.0.0.0:5000/search_user"
     if len(sys.argv) == 2:
