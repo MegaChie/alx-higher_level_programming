2b00da3988ad59c45f592b1989801fdfba0a6497
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-03-01 17:03:30 +0200

    Update 5-hbtn_header.py

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-"""1. Response header value #0"""
+"""5. Response header value #1"""
 import sys
-import urllib.request as urlR
+import requests as requ
 
 
 if __name__ == "__main__":
     """
     Sends a request to the URL and displays the value of the X-Request-Id
+    but this time it uses requests library.
     """
     link = sys.argv[1]
-    with urlR.urlopen(link) as marko:
+    with requ.get(link) as marko:
         polo = marko.headers
         print(polo.get("X-Request-Id"))
