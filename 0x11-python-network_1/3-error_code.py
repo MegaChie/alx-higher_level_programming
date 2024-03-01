cef0cee1e5178334c858294713bcdc5743fd0250
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-03-01 16:38:38 +0200

    Update 3-error_code.py

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """3. Error code #0"""
 import sys
+import urllib.error as urlE
 import urllib.parse as urlP
 import urllib.request as urlR
 
@@ -14,5 +15,5 @@ if __name__ == "__main__":
         with urlR.urlopen(link) as marko:
             polo = marko.read()
             print(polo.decode())
-    except urllib.error.HTTPError as grab:
+    except urlE.HTTPError as grab:
         print("Error code: {}".fromat(grab.code))
