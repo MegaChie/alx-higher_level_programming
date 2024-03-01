7de9b65d5cfc8be0121623f2ac1e6b194529d6ab
MegaChie <workspace.mega@gmail.com>
2024-03-01 06:41:44 -0800

    Add 4-hbtn_status.py

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""3. Error code #0"""
+import sys
+import urllib.error as urlE
+import urllib.parse as urlP
+import urllib.request as urlR
+
+
+if __name__ == "__main__":
+    """
+    sends a request to the URL and displays the body of the response
+    """
+    link = sys.argv[1]
+    try:
+        with urlR.urlopen(link) as marko:
+            polo = marko.read()
+            print(polo.decode())
+    except urlE.HTTPError as grab:
+        print("Error code: {}".format(grab.code))
