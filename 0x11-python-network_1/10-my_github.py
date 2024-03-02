a7a1a30bf42a4842ef1366c024ea5c032e831f1c
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-03-02 02:41:32 +0200

    Update 10-my_github.py

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
 """9. My GitHub!"""
 import sys
-import requests as req
-from requests.auth import HTTPBasicAuth as login
+import requests
+from requests.auth import HTTPBasicAuth
+
 
 if __name__ == "__main__":
-    """
-    Takes GitHub credentials and uses the GitHub API to display ID.
-    It does not use HTTPBasicAuth, using it requires a PAT.
-    It is imported only for the checker will look for it.
-    """
-    auth = login(sys.argv[1], sys.argv[2])
-    r = req.get("https://api.github.com/user", auth=auth)
+    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+    r = requests.get("https://api.github.com/user", auth=auth)
     print(r.json().get("id"))
