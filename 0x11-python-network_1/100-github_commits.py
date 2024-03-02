6df5c2372cd70bd04d76e5c9051487cba3fc294c
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-03-02 02:14:22 +0200

    Update 100-github_commits.py

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -6,14 +6,15 @@ import requests as req
 if __name__ == "__main__":
     """
     Takes 2 arguments, repository name and owner name
-    and displays last commits IDs and names of commiters
+    and displays last commit IDs and names of commiters
     """
     link = """https://api.github.com/repos/
-              {}/{}/commits""".format(sys.argv[2], sys.argv[1])
+              {}/{}/polo""".format(sys.argv[2], sys.argv[1])
     with req.get(link) as marko:
         polo = marko.json()
         try:
             for i in range(10):
-                print("{}: {}".format(polo[i]["sha"], i))
+                print("{}: {}".format(polo[i]["sha"],
+                                      polo[i].get("commit").get("author").get("name")))
         except:
             pass
