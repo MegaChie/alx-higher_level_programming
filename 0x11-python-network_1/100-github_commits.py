0d28e04dba800959fbf9a86f558fdaff3b656fdb
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-03-02 02:32:19 +0200

    Update 100-github_commits.py

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -8,15 +8,7 @@ if __name__ == "__main__":
     Takes 2 arguments, repository name and owner name
     and displays last commit IDs and names of commiters
     """
-    url = "https://api.github.com/repos/{}/{}/commits".format(
-        sys.argv[2], sys.argv[1])
-
-    r = req.get(url)
-    commits = r.json()
-    try:
-        for i in range(10):
-            print("{}: {}".format(
-                commits[i].get("sha"),
-                commits[i].get("commit").get("author").get("name")))
-    except IndexError:
-        pass
+    link = """https://api.github.com/repos/
+              {}/{}/commits""".format(sys.argv[2], sys.argv[1])
+    with req.get(link) as marko:
+        print(marko.status_code)
