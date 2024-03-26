8fd439a67b8fd551ddc7e41e3deebe0d4a6cfa62
MegaChie <workspace.mega@gmail.com>
2024-03-26 05:22:50 -0700

    Update starting count of completed

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -8,7 +8,7 @@ fs.get(ID, function (error, polo, body) {
     const data = JSON.parse(body);
     for (let i = 0; i < data.length; i++) {
       if (data[i].completed) {
-        if (!(data[i].userId in done)) done[data[i].userId] = 0;
+        if (!(data[i].userId in done)) done[data[i].userId] = 1;
         else done[data[i].userId] += 1;
       }
     }
