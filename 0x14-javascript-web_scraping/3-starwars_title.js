e9f7cf47c198e01c13529a6263196ee890c85f9a
MegaChie <workspace.mega@gmail.com>
2024-03-26 15:04:52 -0700

    Make code simpler

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -3,7 +3,7 @@ const base = 'https://swapi-api.alx-tools.com/api/films/';
 const ID = process.argv[2] + '/';
 const link = base.concat(ID);
 const fs = require('request');
-fs.get(link, function (error, polo, body) {
+fs.get(link, (error, polo, body) {
   if (error) throw error;
   console.log(JSON.parse(body).title);
 });
