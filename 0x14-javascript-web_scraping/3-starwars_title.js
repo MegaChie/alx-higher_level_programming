fd0eaabd5a4b72daa25add4e0f98964586770610
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-03-27 00:48:22 +0200

    Less concating

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
-const base = 'https://swapi-api.alx-tools.com/api/films/';
-const ID = process.argv[2] + '/';
-const link = base.concat(ID);
+const base = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2] + '/';
 const fs = require('request');
-fs.get(link, (error, polo, body) => {
+fs.get(base, (error, polo, body) => {
   if (error) throw error;
   console.log(JSON.parse(body).title);
 });
