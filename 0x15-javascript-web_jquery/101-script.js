e2bd46b9a72c67a9aa02dbfeafafe6809703c8d8
MegaChie <113614073+MegaChie@users.noreply.github.com>
2024-04-03 17:17:07 +0200

    Semistandard improvement

--- 0x15-javascript-web_jquery/101-script.js
+++ 0x15-javascript-web_jquery/101-script.js
@@ -1,8 +1,8 @@
 $('document').ready(function () {
   $('div#add_item').on('click', function () {
-    $('UL.my_list').append('<li>Item</li>')
+    $('UL.my_list').append('<li>Item</li>');
   });
-  $('DIV#remove_item').on('click', function() {
+  $('DIV#remove_item').on('click', function () {
     $('UL.my_list').children().last().remove();
   });
   $('DIV#clear_list').on('click', function () {
